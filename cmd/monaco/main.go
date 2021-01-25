52310476f6a62f6273f08855d68f0bc902c20329
Patrick Pichler <patrick.pichler@dynatrace.com>
2021-01-25 08:03:54 +0100

    Move verbose flag to per command for new CLI
    It makes more sense to have the verbose flag on a per command basis as
    on a global level. Hence it has been moved to be on a per command
    basis.

--- cmd/monaco/main.go
+++ cmd/monaco/main.go
@@ -193,13 +193,6 @@ Examples:
   Deploy a specific project to a specific tenant:
     monaco deploy --environments environments.yaml --specific-environment dev --project myProject
 `
-	app.Flags = []cli.Flag{
-		&cli.BoolFlag{
-			Name:    "verbose",
-			Aliases: []string{"v"},
-		},
-	}
-
 	app.Before = func(c *cli.Context) error {
 		err := util.SetupLogging(c.Bool("verbose"))
 
@@ -224,6 +217,10 @@ func getDeployCommand(fileReader util.FileReader) cli.Command {
 		Usage:     "deployes the given environment",
 		UsageText: "deploy [command options] [working directory]",
 		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "verbose",
+				Aliases: []string{"v"},
+			},
 			&cli.PathFlag{
 				Name:      "environments",
 				Usage:     "Yaml file containing environment to deploy to",
@@ -275,6 +272,10 @@ func getDownloadCommand(fileReader util.FileReader) cli.Command {
 		Usage:     "download the given environment",
 		UsageText: "download [command options] [working directory]",
 		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "verbose",
+				Aliases: []string{"v"},
+			},
 			&cli.PathFlag{
 				Name:      "environments",
 				Usage:     "Yaml file containing environment to deploy to",
