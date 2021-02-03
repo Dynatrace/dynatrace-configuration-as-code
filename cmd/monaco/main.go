9a4b42a60b10c9366fdabc6d39acf3b044048e09
Adam Gardner <Adam.Gardner@dynatrace.com>
2021-02-03 18:11:46 +1000

    Remove Git code

--- cmd/monaco/main.go
+++ cmd/monaco/main.go
@@ -212,7 +212,6 @@ func getDeployCommand(fileReader util.FileReader) cli.Command {
 		Usage:     "deploys the given environment",
 		UsageText: "deploy [command options] [working directory]",
 		ArgsUsage: "[working directory]",
-<<<<<<< HEAD
 		Before: func(c *cli.Context) error {
 			err := util.SetupLogging(c.Bool("verbose"))
 
@@ -224,8 +223,6 @@ func getDeployCommand(fileReader util.FileReader) cli.Command {
 
 			return nil
 		},
-=======
->>>>>>> cb214669ad26a04251116de6a2f1a0a54e5dc55e
 		Flags: []cli.Flag{
 			&cli.BoolFlag{
 				Name:    "verbose",
