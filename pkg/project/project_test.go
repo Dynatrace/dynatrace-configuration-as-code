3fa27f9174d13d6be9c860964bea34c20e1b2987
Wojciech Stanislawski <wojciech.stanislawski@dynatrace.com>
2021-03-22 15:55:02 +0100

    Fixed failing tests

--- pkg/project/project_test.go
+++ pkg/project/project_test.go
@@ -60,9 +60,9 @@ func createTestApis() map[string]api.Api {
 	return apis
 }
 
-var testAlertingProfileApi = api.NewStandardApi("alerting-profile", "/api/config/v1/alertingProfiles", false)
-var testManagementZoneApi = api.NewStandardApi("management-zone", "/api/config/v1/managementZones", false)
-var testDashboardApi = api.NewStandardApi("dashboard", "/api/config/v1/dashboards", false)
+var testAlertingProfileApi = api.NewStandardApi("alerting-profile", "/api/config/v1/alertingProfiles", false, false, false)
+var testManagementZoneApi = api.NewStandardApi("management-zone", "/api/config/v1/managementZones", false, false, false)
+var testDashboardApi = api.NewStandardApi("dashboard", "/api/config/v1/dashboards", false, false, false)
 
 func TestGetPathSuccess(t *testing.T) {
 
