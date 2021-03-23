00f42a14b80bdd8a53ed64af176a32d166dbfe03
Marek.Kapusto <marek.kapusto@dynatrace.com>
2021-03-23 12:51:28 +0100

    test fix

--- pkg/project/project_test.go
+++ pkg/project/project_test.go
@@ -60,9 +60,9 @@ func createTestApis() map[string]api.Api {
 	return apis
 }
 
-var testAlertingProfileApi = api.NewStandardApi("alerting-profile", "/api/config/v1/alertingProfiles", "", false, false)
-var testManagementZoneApi = api.NewStandardApi("management-zone", "/api/config/v1/managementZones", "", false, false)
-var testDashboardApi = api.NewStandardApi("dashboard", "/api/config/v1/dashboards", "", false, false)
+var testAlertingProfileApi = api.NewStandardApi("alerting-profile", "/api/config/v1/alertingProfiles", "", false, false, nil, nil)
+var testManagementZoneApi = api.NewStandardApi("management-zone", "/api/config/v1/managementZones", "", false, false, nil, nil)
+var testDashboardApi = api.NewStandardApi("dashboard", "/api/config/v1/dashboards", "", false, false, nil, nil)
 
 func TestGetPathSuccess(t *testing.T) {
 
