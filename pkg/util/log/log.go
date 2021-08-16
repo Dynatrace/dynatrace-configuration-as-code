56a6a2dac48f8dab9e0dd10749c4b3a19b66af62
Patrick Berchtold <patrick.berchtold@dynatrace.com>
2021-08-16 10:53:17 +0200

    feat: Change structure of log package
    Create a new extendedLogger struct, which serves as a container for the
    underlying loggers.
    There still is a default implementation which will be called by the
    package functions.
    
    Create an enum for the logLevel, which can be set instead of just a bool
    for verbose.

--- pkg/util/log/log.go
+++ pkg/util/log/log.go
@@ -25,21 +25,104 @@ import (
 )
 
 const (
-	prefixInfo  = "INFO  "
-	prefixWarn  = "WARN  "
-	prefixError = "ERROR "
 	prefixFatal = "FATAL "
+	prefixError = "ERROR "
+	prefixWarn  = "WARN  "
+	prefixInfo  = "INFO  "
 	prefixDebug = "DEBUG "
 )
 
-var (
-	// Verbose controls if logs at DEBUG level are printed to stdout.
-	// Default: false
-	Verbose bool = false
+type logLevel int
 
-	consoleLogger *log.Logger = log.Default()
-	fileLogger    *log.Logger
+const (
+	LevelFatal logLevel = iota
+	LevelError
+	LevelWarn
+	LevelInfo
+	LevelDebug
 )
+
+func (l logLevel) prefix() string {
+	switch l {
+	case LevelFatal:
+		return prefixFatal
+	case LevelError:
+		return prefixError
+	case LevelWarn:
+		return prefixWarn
+	case LevelInfo:
+		return prefixInfo
+	case LevelDebug:
+		return prefixDebug
+	}
+	return ""
+}
+
+type extendedLogger struct {
+	consoleLogger *log.Logger
+	fileLogger    *log.Logger
+	level         logLevel
+}
+
+// New creates a new extendedLogger, which contains two
+// underlying loggers, a console logger and a file logger.
+// The file logger will always print all logs, while the
+// console logger will only print logs according to the
+// level of this logger, e.g. at LevelInfo.
+func New(consoleLogger, fileLogger *log.Logger, level logLevel) *extendedLogger {
+	return &extendedLogger{
+		consoleLogger: consoleLogger,
+		fileLogger:    fileLogger,
+		level:         level,
+	}
+}
+
+// Fatal logs the message with the prefix FATAL.
+func (l *extendedLogger) Fatal(msg string, a ...interface{}) {
+	doLog(l, LevelFatal, msg, a...)
+}
+
+// Error logs the message with the prefix ERROR.
+func (l *extendedLogger) Error(msg string, a ...interface{}) {
+	doLog(l, LevelError, msg, a...)
+}
+
+// Warn logs the message with the prefix WARN.
+func (l *extendedLogger) Warn(msg string, a ...interface{}) {
+	doLog(l, LevelError, msg, a...)
+}
+
+// Info logs the message with the prefix INFO.
+func (l *extendedLogger) Info(msg string, a ...interface{}) {
+	doLog(l, LevelInfo, msg, a...)
+}
+
+// Debug logs the message with the prefix DEBUG.
+func (l *extendedLogger) Debug(msg string, a ...interface{}) {
+	doLog(l, LevelDebug, msg, a...)
+}
+
+// SetLevel sets the log level for this logger. This
+// influences only the console logger, the file logger
+// will always have the highest level regardless.
+func (l *extendedLogger) SetLevel(level logLevel) {
+	l.level = level
+}
+
+var defaultLogger = &extendedLogger{
+	consoleLogger: log.Default(),
+	level:         LevelInfo,
+}
+
+// Default returns the default logger which is used, when
+// functions like Info() and Debug() are called. The
+// console logger will be the same default logger from
+// the standard library, the level will be INFO and
+// there's no file logger set up. The function
+// SetupLogging() can be used to create a file logger.
+func Default() *extendedLogger {
+	return defaultLogger
+}
 
 // SetupLogging is used to enable file logging, including
 // Request and Response logs. If logging functions are
@@ -66,49 +149,47 @@ func setupFileLogging() error {
 	if err != nil {
 		return fmt.Errorf("unable to create file logger: %v", err)
 	}
-	fileLogger = log.New(logFile, "", log.LstdFlags)
+	defaultLogger.fileLogger = log.New(logFile, "", log.LstdFlags)
 	return nil
 }
 
-// Info logs the message with the prefix INFO.
-func Info(msg string, a ...interface{}) {
-	doLog(msg, prefixInfo, a...)
-}
-
-// Warn logs the message with the prefix WARN.
-func Warn(msg string, a ...interface{}) {
-	doLog(msg, prefixWarn, a...)
-}
-
-// Error logs the message with the prefix ERROR.
-func Error(msg string, a ...interface{}) {
-	doLog(msg, prefixError, a...)
-}
-
-// Fatal logs the message with the prefix FATAL.
+// Fatal logs the message with the prefix FATAL to the
+// default logger (see Default)).
 func Fatal(msg string, a ...interface{}) {
-	doLog(msg, prefixFatal, a...)
+	defaultLogger.Fatal(msg, a...)
 }
 
-// Debug logs the message with the prefix DEBUG.
-// If verbose is set to true, this behaves like any other
-// logging level, otherwise only the file logger will be
-// used and no prints to the default logger will be done.
+// Error logs the message with the prefix ERROR to the
+// default logger (see Default)).
+func Error(msg string, a ...interface{}) {
+	defaultLogger.Error(msg, a...)
+}
+
+// Warn logs the message with the prefix WARN to the
+// default logger (see Default()).
+func Warn(msg string, a ...interface{}) {
+	defaultLogger.Warn(msg, a...)
+}
+
+// Info logs the message with the prefix INFO to the
+// default logger (see Default()).
+func Info(msg string, a ...interface{}) {
+	defaultLogger.Info(msg, a...)
+}
+
+// Debug logs the message with the prefix DEBUG to the
+// default logger (see Default()).
 func Debug(msg string, a ...interface{}) {
-	if Verbose {
-		doLog(msg, prefixDebug, a...)
-		return
-	}
-	if fileLogger != nil {
-		fileLogger.Printf(prefixDebug+msg, a...)
-	}
+	defaultLogger.Debug(msg, a...)
 }
 
-func doLog(msg, prefix string, a ...interface{}) {
-	msg = fmt.Sprintf(prefix+msg, a...)
-	consoleLogger.Println(msg)
-	if fileLogger != nil {
-		fileLogger.Println(msg)
+func doLog(logger *extendedLogger, level logLevel, msg string, a ...interface{}) {
+	msg = fmt.Sprintf(level.prefix()+msg, a...)
+	if logger.level >= level && logger.consoleLogger != nil {
+		logger.consoleLogger.Println(msg)
+	}
+	if logger.fileLogger != nil {
+		logger.fileLogger.Println(msg)
 	}
 }
 
