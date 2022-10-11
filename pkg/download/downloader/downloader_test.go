0ca8d104d15bd937b2cacfbd1811979782c3db94
David Laubreiter <david.laubreiter@dynatrace.com>
2022-10-11 10:58:27 +0200

    test(download): Add more tests for downloader
    Signed-off-by: David Laubreiter <david.laubreiter@dynatrace.com>

--- pkg/download/downloader/downloader_test.go
+++ pkg/download/downloader/downloader_test.go
@@ -17,9 +17,11 @@
 package downloader
 
 import (
+	"errors"
 	"github.com/dynatrace-oss/dynatrace-monitoring-as-code/pkg/api"
 	config "github.com/dynatrace-oss/dynatrace-monitoring-as-code/pkg/config/v2"
 	"github.com/dynatrace-oss/dynatrace-monitoring-as-code/pkg/rest"
+	"github.com/golang/mock/gomock"
 	"gotest.tools/assert"
 	"os"
 	"testing"
@@ -137,5 +139,148 @@ func TestDownloadAllConfigs(t *testing.T) {
 		configs, found = configsPerApi["api-3"]
 		assert.Equal(t, found, true, "api should be present in the result")
 		assert.DeepEqual(t, configs, []config.Config{c1, c2})
+	})
+}
+
+func TestDownloadConfigForApi(t *testing.T) {
+
+	t.Run("find configs returns an error and empty configs are returned", func(t *testing.T) {
+		a, _ := api.CreateAPIMockWithId(t, "api-1")
+
+		var find findConfigsToDownloadFunc = func(currentApi api.Api, client rest.DynatraceClient) ([]api.Value, error) {
+			return []api.Value{}, errors.New("some-reason")
+		}
+
+		var filter filterConfigsToSkipFunc = func(a api.Api, values []api.Value) []api.Value {
+			t.Error("filter should never be called")
+			return nil
+		}
+
+		var download downloadConfigsOfApiFunc = func(a api.Api, values []api.Value, client rest.DynatraceClient, s string) []config.Config {
+			t.Error("download should never be called")
+			return nil
+		}
+
+		configs := downloadConfigForApi(a, nil, "", find, filter, download)
+		assert.Equal(t, len(configs), 0, "empty array should be returned")
+	})
+
+	t.Run("if filter filters nothing, an empty array is returned", func(t *testing.T) {
+		a, _ := api.CreateAPIMockWithId(t, "api-1")
+
+		vals := []api.Value{{}, {}}
+
+		var filterCalled bool // check that filter actually has been called
+
+		var find findConfigsToDownloadFunc = func(currentApi api.Api, client rest.DynatraceClient) ([]api.Value, error) {
+			return vals, nil
+		}
+
+		var filter filterConfigsToSkipFunc = func(a api.Api, values []api.Value) []api.Value {
+			filterCalled = true
+			assert.DeepEqual(t, values, vals)
+			return []api.Value{}
+		}
+
+		var download downloadConfigsOfApiFunc = func(a api.Api, values []api.Value, client rest.DynatraceClient, s string) []config.Config {
+			return nil
+		}
+
+		configs := downloadConfigForApi(a, nil, "", find, filter, download)
+		assert.Equal(t, filterCalled, true, "filter function has not been called")
+		assert.Equal(t, len(configs), 0, "configs should be empty")
+	})
+
+	t.Run("download is called with the correct values", func(t *testing.T) {
+		a, _ := api.CreateAPIMockWithId(t, "api-1")
+
+		vals := []api.Value{{}, {}, {}}
+		confs := []config.Config{{}, {}, {}}
+
+		var find findConfigsToDownloadFunc = func(currentApi api.Api, client rest.DynatraceClient) ([]api.Value, error) {
+			return vals, nil
+		}
+
+		var filter filterConfigsToSkipFunc = func(a api.Api, values []api.Value) []api.Value {
+			return vals
+		}
+
+		var download downloadConfigsOfApiFunc = func(a api.Api, values []api.Value, client rest.DynatraceClient, s string) []config.Config {
+			assert.DeepEqual(t, values, vals)
+			return confs
+		}
+
+		configs := downloadConfigForApi(a, nil, "", find, filter, download)
+		assert.DeepEqual(t, configs, confs)
+	})
+
+	t.Run("project name and client is forwarded correctly", func(t *testing.T) {
+		a, _ := api.CreateAPIMockWithId(t, "api-1")
+		c := rest.CreateDynatraceClientMockFactory(t)
+
+		var downloadHasBeenCalled bool
+
+		var find findConfigsToDownloadFunc = func(currentApi api.Api, client rest.DynatraceClient) ([]api.Value, error) {
+			return nil, nil
+		}
+
+		var filter filterConfigsToSkipFunc = func(a api.Api, values []api.Value) []api.Value {
+			return []api.Value{{}}
+		}
+
+		var download downloadConfigsOfApiFunc = func(a api.Api, values []api.Value, client rest.DynatraceClient, s string) []config.Config {
+			assert.Equal(t, a.GetId(), "api-1")
+			assert.Equal(t, s, "project-name")
+			assert.Equal(t, client, c)
+
+			downloadHasBeenCalled = true
+
+			return nil
+		}
+
+		downloadConfigForApi(a, c, "project-name", find, filter, download)
+		assert.Equal(t, downloadHasBeenCalled, true, "download has not been called")
+	})
+}
+
+func TestFindConfigsToDownload(t *testing.T) {
+	t.Run("singleton-apis return the config without invoking the client", func(t *testing.T) {
+		c := rest.NewMockDynatraceClient(gomock.NewController(t))
+
+		a := api.NewMockApi(gomock.NewController(t))
+		a.EXPECT().IsSingleConfigurationApi().Return(true)
+		a.EXPECT().GetId().AnyTimes().Return("api-id")
+
+		download, err := findConfigsToDownload(a, c)
+
+		assert.NilError(t, err)
+		assert.DeepEqual(t, download, []api.Value{{Id: "api-id", Name: "api-id"}})
+	})
+
+	t.Run("non-singletons fetch values from the client and return them", func(t *testing.T) {
+		vals := []api.Value{{}}
+
+		a := api.NewMockApi(gomock.NewController(t))
+		a.EXPECT().IsSingleConfigurationApi().Return(false)
+		a.EXPECT().GetId().AnyTimes().Return("api-id")
+
+		c := rest.NewMockDynatraceClient(gomock.NewController(t))
+		c.EXPECT().List(a).Return(vals, nil)
+
+		download, err := findConfigsToDownload(a, c)
+		assert.NilError(t, err)
+		assert.DeepEqual(t, download, vals)
+	})
+
+	t.Run("non-singletons fetch errors are returned", func(t *testing.T) {
+		a := api.NewMockApi(gomock.NewController(t))
+		a.EXPECT().IsSingleConfigurationApi().Return(false)
+		a.EXPECT().GetId().AnyTimes().Return("api-id")
+
+		c := rest.NewMockDynatraceClient(gomock.NewController(t))
+		c.EXPECT().List(a).Return(nil, errors.New("error"))
+
+		_, err := findConfigsToDownload(a, c)
+		assert.Error(t, err, "error")
 	})
 }
