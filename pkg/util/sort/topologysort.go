ee8fa7294502f1b42d060a2373fed0fb54f5ec9c
Nicola Riedmann <nicola.riedmann@dynatrace.com>
2022-11-10 13:45:57 +0100

    docs(sort): Add doc comments to topologysort

--- pkg/util/sort/topologysort.go
+++ pkg/util/sort/topologysort.go
@@ -16,6 +16,9 @@ package sort
 
 import "fmt"
 
+// TopologySortError is an error returned for any unresolved dependency after sorting
+// The error marks the ID of the node left with unresolved incoming edges after sorting,
+// as well as the IDs of the nodes still pointing to it.
 type TopologySortError struct {
 	OnId                        int
 	UnresolvedIncomingEdgesFrom []int
@@ -29,7 +32,20 @@ var (
 	_ error = (*TopologySortError)(nil)
 )
 
-// https://en.wikipedia.org/wiki/Topological_sorting#Kahn's_algorithm
+// TopologySort implements [Kahn's algorithm for topological sorting]
+// As an input to the algorithm it takes a directed graph represented as a dependency matrix of incoming edges.
+// To simplify checking full resolution/sort failure a precomputed slice of inDegrees per node must be provided as well.
+// Indices in the incomingEdges dependency matrix and inDegrees slice need to match - e.g. if incomingEdges[i] marks two
+// edges as 'true', inDegrees[i] must equal 2.
+//
+// Successful sorting - which is guaranteed for acyclic directed graphs - will see all edges resolved,
+// leaving each node with an inDegree of zero.
+//
+// In case of cycles in the graph some nodes will be left with unresolved edges (inDegree > 0) - in this case a list of
+// [TopologySortError] will be returned, marking each node id with unresolved incoming edges and which node ids are still
+// pointing to it.
+//
+// [Kahn's algorithm for topological sorting]: https://en.wikipedia.org/wiki/Topological_sorting#Kahn's_algorithm
 func TopologySort(incomingEdges [][]bool, inDegrees []int) (topoSorted []int, errs []TopologySortError) {
 
 	nodes := getAllLeaves(inDegrees)
@@ -63,6 +79,7 @@ func TopologySort(incomingEdges [][]bool, inDegrees []int) (topoSorted []int, er
 	return topoSorted, errs
 }
 
+// getAllLeaves returns all leaves in the graph to sort - by returning all node ids with no incoming edges
 func getAllLeaves(inDegrees []int) []int {
 	var nodes []int
 	for i := range inDegrees {
@@ -73,6 +90,7 @@ func getAllLeaves(inDegrees []int) []int {
 	return nodes
 }
 
+// getSourceOfIncomingEdges transforms a row of the dependency matrix into the ids of all nodes that are the source of an incoming edge
 func getSourceOfIncomingEdges(incomingEdges []bool) []int {
 	var sources []int
 	for i, hasEdge := range incomingEdges {
