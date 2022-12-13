62a9f71ab8e43c62b4e39c8df9fc35b3b1b6ec12
David Laubreiter <david.laubreiter@dynatrace.com>
2022-12-13 14:40:40 +0100

    test: Add integration tests for scope-parameters

---
+++ cmd/monaco/integrationtest/v2/scope_paramter_integration_test.go
@@ -0,0 +1,63 @@
+//go:build integration
+
+/**
+ * @license
+ * Copyright 2020 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2
+
+import (
+	"github.com/dynatrace-oss/dynatrace-monitoring-as-code/cmd/monaco/runner"
+	"github.com/dynatrace-oss/dynatrace-monitoring-as-code/pkg/util"
+	"github.com/spf13/afero"
+	"gotest.tools/assert"
+	"testing"
+)
+
+func TestIntegrationScopeParameters(t *testing.T) {
+	configFolder := "test-resources/integration-scope-parameters/"
+	manifest := configFolder + "/manifest.yaml"
+	specificEnvironment := ""
+
+	RunIntegrationWithCleanup(t, configFolder, manifest, specificEnvironment, "SettingsTwo", func(fs afero.Fs) {
+
+		// This causes Creation of all Settings
+		cmd := runner.BuildCli(fs)
+		cmd.SetArgs([]string{"deploy", "--verbose", manifest})
+		err := cmd.Execute()
+
+		assert.NilError(t, err)
+
+		// This causes an Update of all Settings
+		cmd = runner.BuildCli(fs)
+		cmd.SetArgs([]string{"deploy", "--verbose", manifest})
+		err = cmd.Execute()
+		assert.NilError(t, err)
+
+	})
+}
+
+// Tests a dry run (validation)
+func TestIntegrationScopeParameterValidation(t *testing.T) {
+
+	configFolder := "test-resources/integration-scope-parameters/"
+	manifest := configFolder + "manifest.yaml"
+
+	cmd := runner.BuildCli(util.CreateTestFileSystem())
+	cmd.SetArgs([]string{"deploy", "--verbose", "--dry-run", manifest})
+	err := cmd.Execute()
+
+	assert.NilError(t, err)
+}
