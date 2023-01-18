509caf1b19878fd088b3cd2279e658589bef8ead
Bernd Warmuth <bernd.warmuth@dynatrace.com>
2023-01-18 13:42:03 +0100

    chore: restructured unit tests

--- pkg/download/classic/download_filter_test.go
+++ pkg/download/classic/download_filter_test.go
@@ -34,53 +34,104 @@ func Test_AllDefinedApiFiltersHaveApis(t *testing.T) {
 	}
 }
 
-func Test_APIFiltersLogic(t *testing.T) {
-	assert.False(t, apiFilters["dashboard"].shouldConfigBePersisted(map[string]interface{}{
-		"dashboardMetadata": map[string]interface{}{
-			"preset": true,
-		},
-	}))
-	assert.True(t, apiFilters["dashboard"].shouldConfigBePersisted(map[string]interface{}{
-		"dashboardMetadata": map[string]interface{}{
-			"preset": false,
-		},
-	}))
+func Test_ShouldBePersisted(t *testing.T) {
+	t.Run("dashboard -  should not be persisted if its a preset", func(t *testing.T) {
+		assert.False(t, apiFilters["dashboard"].shouldConfigBePersisted(map[string]interface{}{
+			"dashboardMetadata": map[string]interface{}{
+				"preset": true,
+			},
+		}))
+	})
 
-	owner := "Dynatrace"
-	owner2 := "NotDynatrace"
-	assert.True(t, apiFilters["dashboard"].shouldBeSkippedPreDownload(api.Value{Owner: &owner}))
-	assert.False(t, apiFilters["dashboard"].shouldBeSkippedPreDownload(api.Value{Owner: &owner2}))
-	assert.False(t, apiFilters["dashboard"].shouldBeSkippedPreDownload(api.Value{}))
+	t.Run("dashboard -  should be persisted if dashboardMetadata is missing", func(t *testing.T) {
+		assert.True(t, apiFilters["dashboard"].shouldConfigBePersisted(map[string]interface{}{}))
+	})
 
-	assert.True(t, apiFilters["synthetic-location"].shouldConfigBePersisted(map[string]interface{}{
-		"type": "PRIVATE",
-	}))
-	assert.False(t, apiFilters["synthetic-location"].shouldConfigBePersisted(map[string]interface{}{
-		"type": "NOT-PRIVATE",
-	}))
+	t.Run("dashboard -  should be persisted if it's not a preset", func(t *testing.T) {
+		assert.True(t, apiFilters["dashboard"].shouldConfigBePersisted(map[string]interface{}{
+			"dashboardMetadata": map[string]interface{}{
+				"preset": false,
+			},
+		}))
+	})
 
-	assert.True(t, apiFilters["hosts-auto-update"].shouldConfigBePersisted(map[string]interface{}{}))
-	assert.True(t, apiFilters["hosts-auto-update"].shouldConfigBePersisted(map[string]interface{}{
-		"updateWindows": map[string]interface{}{},
-	}))
-	assert.False(t, apiFilters["hosts-auto-update"].shouldConfigBePersisted(map[string]interface{}{
-		"updateWindows": map[string]interface{}{
-			"windows": []interface{}{},
-		},
-	}))
-	assert.True(t, apiFilters["hosts-auto-update"].shouldConfigBePersisted(map[string]interface{}{
-		"updateWindows": map[string]interface{}{
-			"windows": []interface{}{"some-entry"},
-		},
-	}))
+	t.Run("dashboard -  should be persisted if dashboardMetadata.preset is missing", func(t *testing.T) {
+		assert.True(t, apiFilters["dashboard"].shouldConfigBePersisted(map[string]interface{}{
+			"dashboardMetadata": map[string]interface{}{},
+		}))
+	})
 
-	assert.True(t, apiFilters["anomaly-detection-metrics"].shouldBeSkippedPreDownload(api.Value{
-		Id: "dynatrace.",
-	}))
-	assert.True(t, apiFilters["anomaly-detection-metrics"].shouldBeSkippedPreDownload(api.Value{
-		Id: "ruxit.",
-	}))
-	assert.False(t, apiFilters["anomaly-detection-metrics"].shouldBeSkippedPreDownload(api.Value{
-		Id: "somtehing.",
-	}))
+	t.Run("synthetic-location - should be persisted if it's private", func(t *testing.T) {
+		assert.True(t, apiFilters["synthetic-location"].shouldConfigBePersisted(map[string]interface{}{
+			"type": "PRIVATE",
+		}))
+	})
+
+	t.Run("synthetic-location - should not be persisted if it's public", func(t *testing.T) {
+		assert.False(t, apiFilters["synthetic-location"].shouldConfigBePersisted(map[string]interface{}{
+			"type": "PUBLIC",
+		}))
+	})
+
+	t.Run("hosts-auto-update - Empty update windows are not persisted", func(t *testing.T) {
+		assert.False(t, apiFilters["hosts-auto-update"].shouldConfigBePersisted(map[string]interface{}{
+			"updateWindows": map[string]interface{}{
+				"windows": []interface{}{},
+			},
+		}))
+	})
+
+	t.Run("hosts-auto-update - Missing update windows are is persisted", func(t *testing.T) {
+		assert.True(t, apiFilters["hosts-auto-update"].shouldConfigBePersisted(map[string]interface{}{}))
+	})
+
+	t.Run("hosts-auto-update - Windows with values are persisted", func(t *testing.T) {
+		assert.True(t, apiFilters["hosts-auto-update"].shouldConfigBePersisted(map[string]interface{}{
+			"updateWindows": map[string]interface{}{
+				"windows": []interface{}{"1", "2", "3"},
+			},
+		}))
+	})
+
+}
+
+func TestShouldConfigBeSkipped(t *testing.T) {
+
+	t.Run("dashboard - Owner 'Dynatrace' is skipped", func(t *testing.T) {
+		owner := "Dynatrace"
+		assert.True(t, apiFilters["dashboard"].shouldBeSkippedPreDownload(api.Value{Owner: &owner}))
+	})
+
+	t.Run("dashboard - Owner 'Not Dynatrace' is not skipped", func(t *testing.T) {
+		owner := "Not Dynatrace"
+		assert.False(t, apiFilters["dashboard"].shouldBeSkippedPreDownload(api.Value{Owner: &owner}))
+	})
+
+	t.Run("dashboard - No owner is not skipped", func(t *testing.T) {
+		assert.False(t, apiFilters["dashboard"].shouldBeSkippedPreDownload(api.Value{}))
+	})
+
+	t.Run("anomaly-detection-metrics - ruxit. should be skipped", func(t *testing.T) {
+		assert.True(t, apiFilters["anomaly-detection-metrics"].shouldBeSkippedPreDownload(api.Value{
+			Id: "ruxit.",
+		}))
+	})
+
+	t.Run("anomaly-detection-metrics - dynatrace. should be skipped", func(t *testing.T) {
+		assert.True(t, apiFilters["anomaly-detection-metrics"].shouldBeSkippedPreDownload(api.Value{
+			Id: "dynatrace.",
+		}))
+	})
+
+	t.Run("anomaly-detection-metrics - ids should not be skipped", func(t *testing.T) {
+		assert.False(t, apiFilters["anomaly-detection-metrics"].shouldBeSkippedPreDownload(api.Value{
+			Id: "b836ff25-24e3-496d-8dce-d94110815ab5",
+		}))
+	})
+
+	t.Run("anomaly-detection-metrics - random strings should not be skipped", func(t *testing.T) {
+		assert.False(t, apiFilters["anomaly-detection-metrics"].shouldBeSkippedPreDownload(api.Value{
+			Id: "test.something",
+		}))
+	})
 }
