f80249a3a488dc6bb2164d6fe186681b167847f3
Jure Skelin <jure.skelin@dynatrace.com>
2023-01-19 10:24:22 +0100

    feat: add information for config api deprecated by settings api

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -27,13 +27,16 @@ var apiMap = map[string]apiInput{
 
 	"alerting-profile": {
 		apiPath:            "/api/config/v1/alertingProfiles",
+		deprecatedBy:       "builtin:alerting.profile",
 		isNonUniqueNameApi: true,
 	},
 	"management-zone": {
-		apiPath: "/api/config/v1/managementZones",
+		apiPath:      "/api/config/v1/managementZones",
+		deprecatedBy: "builtin:management-zones",
 	},
 	"auto-tag": {
-		apiPath: "/api/config/v1/autoTags",
+		apiPath:      "/api/config/v1/autoTags",
+		deprecatedBy: "builtin:tags.auto-tagging",
 	},
 	"dashboard": {
 		apiPath:                      "/api/config/v1/dashboards",
@@ -41,7 +44,8 @@ var apiMap = map[string]apiInput{
 		isNonUniqueNameApi:           true,
 	},
 	"notification": {
-		apiPath: "/api/config/v1/notifications",
+		apiPath:      "/api/config/v1/notifications",
+		deprecatedBy: "builtin:problem.notifications",
 	},
 	"extension": {
 		apiPath:                      "/api/config/v1/extensions",
@@ -69,11 +73,13 @@ var apiMap = map[string]apiInput{
 	},
 	"anomaly-detection-metrics": {
 		apiPath:            "/api/config/v1/anomalyDetection/metricEvents",
+		deprecatedBy:       "builtin:anomaly-detection.metric-events",
 		isNonUniqueNameApi: true,
 	},
 	// Early adopter API !
 	"anomaly-detection-disks": {
-		apiPath: "/api/config/v1/anomalyDetection/diskEvents",
+		apiPath:      "/api/config/v1/anomalyDetection/diskEvents",
+		deprecatedBy: "builtin:anomaly-detection.infrastructure-disks",
 	},
 	// Environment API not Config API
 	"synthetic-location": {
@@ -90,7 +96,8 @@ var apiMap = map[string]apiInput{
 		apiPath: "/api/config/v1/applications/mobile",
 	},
 	"app-detection-rule": {
-		apiPath: "/api/config/v1/applicationDetectionRules",
+		apiPath:      "/api/config/v1/applicationDetectionRules",
+		deprecatedBy: "builtin:rum.web.app-detection",
 	},
 	"aws-credentials": {
 		apiPath:      "/api/config/v1/aws/credentials",
@@ -98,7 +105,8 @@ var apiMap = map[string]apiInput{
 	},
 	// Early adopter API !
 	"kubernetes-credentials": {
-		apiPath: "/api/config/v1/kubernetes/credentials",
+		apiPath:      "/api/config/v1/kubernetes/credentials",
+		deprecatedBy: "builtin:cloud.kubernetes",
 		//isNonUniqueNameApi: true, // non-unique name handling for k8s credentials does not work, as path ID needs to be a ME-ID not a uuid; handling as unique again for now
 		skipDownload: true,
 	},
@@ -107,7 +115,8 @@ var apiMap = map[string]apiInput{
 		skipDownload: true,
 	},
 	"request-attributes": {
-		apiPath: "/api/config/v1/service/requestAttributes",
+		apiPath:      "/api/config/v1/service/requestAttributes",
+		deprecatedBy: "builtin:request-attributes",
 	},
 	"calculated-metrics-service": {
 		apiPath: "/api/config/v1/calculatedMetrics/service",
@@ -135,10 +144,12 @@ var apiMap = map[string]apiInput{
 		apiPath: "/api/config/v1/conditionalNaming/service",
 	},
 	"maintenance-window": {
-		apiPath: "/api/config/v1/maintenanceWindows",
+		apiPath:      "/api/config/v1/maintenanceWindows",
+		deprecatedBy: "builtin:alerting.maintenance-window",
 	},
 	"request-naming-service": {
-		apiPath:            "/api/config/v1/service/requestNaming",
+		apiPath: "/api/config/v1/service/requestNaming",
+		//deprecatedBy: "builtin:unified-request-name-ruleset", //not yet in production
 		isNonUniqueNameApi: true,
 	},
 	// Environment API not Config API
@@ -152,22 +163,28 @@ var apiMap = map[string]apiInput{
 		skipDownload:                 true,
 	},
 	"failure-detection-parametersets": {
-		apiPath: "/api/config/v1/service/failureDetection/parameterSelection/parameterSets",
+		apiPath:      "/api/config/v1/service/failureDetection/parameterSelection/parameterSets",
+		deprecatedBy: "builtin:failure-detection.environment.parameters",
 	},
 	"failure-detection-rules": {
-		apiPath: "/api/config/v1/service/failureDetection/parameterSelection/rules",
+		apiPath:      "/api/config/v1/service/failureDetection/parameterSelection/rules",
+		deprecatedBy: "builtin:failure-detection.environment.rules",
 	},
 	"service-detection-full-web-request": {
-		apiPath: "/api/config/v1/service/detectionRules/FULL_WEB_REQUEST",
+		apiPath:      "/api/config/v1/service/detectionRules/FULL_WEB_REQUEST",
+		deprecatedBy: "builtin:service-detection.full-web-request",
 	},
 	"service-detection-full-web-service": {
-		apiPath: "/api/config/v1/service/detectionRules/FULL_WEB_SERVICE",
+		apiPath:      "/api/config/v1/service/detectionRules/FULL_WEB_SERVICE",
+		deprecatedBy: "builtin:service-detection.full-web-service",
 	},
 	"service-detection-opaque-web-request": {
-		apiPath: "/api/config/v1/service/detectionRules/OPAQUE_AND_EXTERNAL_WEB_REQUEST",
+		apiPath:      "/api/config/v1/service/detectionRules/OPAQUE_AND_EXTERNAL_WEB_REQUEST",
+		deprecatedBy: "builtin:service-detection.external-web-request",
 	},
 	"service-detection-opaque-web-service": {
-		apiPath: "/api/config/v1/service/detectionRules/OPAQUE_AND_EXTERNAL_WEB_SERVICE",
+		apiPath:      "/api/config/v1/service/detectionRules/OPAQUE_AND_EXTERNAL_WEB_SERVICE",
+		deprecatedBy: "builtin:service-detection.external-web-service",
 	},
 	// Early adopter API !
 	"reports": {
@@ -175,62 +192,77 @@ var apiMap = map[string]apiInput{
 	},
 	"frequent-issue-detection": {
 		apiPath:                  "/api/config/v1/frequentIssueDetection",
+		deprecatedBy:             "builtin:anomaly-detection.frequent-issues",
 		isSingleConfigurationApi: true,
 	},
 	"data-privacy": {
 		apiPath:                  "/api/config/v1/dataPrivacy",
+		deprecatedBy:             "builtin:preferences.privacy",
 		isSingleConfigurationApi: true,
 	},
 	"hosts-auto-update": {
 		apiPath:                  "/api/config/v1/hosts/autoupdate",
+		deprecatedBy:             "builtin:deployment.oneagent.updates",
 		isSingleConfigurationApi: true,
 	},
 	"anomaly-detection-applications": {
 		apiPath:                  "/api/config/v1/anomalyDetection/applications",
+		deprecatedBy:             "builtin:anomaly-detection.rum-web / -mobile",
 		isSingleConfigurationApi: true,
 	},
 	"anomaly-detection-aws": {
 		apiPath:                  "/api/config/v1/anomalyDetection/aws",
+		deprecatedBy:             "builtin:anomaly-detection.infrastructure-aws",
 		isSingleConfigurationApi: true,
 	},
 	"anomaly-detection-database-services": {
 		apiPath:                  "/api/config/v1/anomalyDetection/databaseServices",
+		deprecatedBy:             "builtin:anomaly-detection.databases",
 		isSingleConfigurationApi: true,
 	},
 	"anomaly-detection-hosts": {
 		apiPath:                  "/api/config/v1/anomalyDetection/hosts",
+		deprecatedBy:             "builtin:anomaly-detection.infrastructure-hosts",
 		isSingleConfigurationApi: true,
 	},
 	"anomaly-detection-services": {
 		apiPath:                  "/api/config/v1/anomalyDetection/services",
+		deprecatedBy:             "builtin:anomaly-detection.services",
 		isSingleConfigurationApi: true,
 	},
 	"anomaly-detection-vmware": {
 		apiPath:                  "/api/config/v1/anomalyDetection/vmware",
+		deprecatedBy:             "builtin:anomaly-detection.infrastructure-vmware",
 		isSingleConfigurationApi: true,
 	},
 	"service-resource-naming": {
-		apiPath:                  "/api/config/v1/service/resourceNaming",
+		apiPath: "/api/config/v1/service/resourceNaming",
+		//deprecatedBy:             "builtin:unified-request-name-ruleset", //not yet in production
 		isSingleConfigurationApi: true,
 	},
 	"app-detection-rule-host": {
 		apiPath:                  "/api/config/v1/applicationDetectionRules/hostDetection",
+		deprecatedBy:             "builtin:rum.host-headers",
 		isSingleConfigurationApi: true,
 	},
 	"content-resources": {
 		apiPath:                  "/api/config/v1/contentResources",
+		deprecatedBy:             "builtin:rum.provider-breakdown",
 		isSingleConfigurationApi: true,
 	},
 	"allowed-beacon-origins": {
 		apiPath:                  "/api/config/v1/allowedBeaconOriginsForCors",
+		deprecatedBy:             "builtin:rum.web.beacon-domain-origins",
 		isSingleConfigurationApi: true,
 	},
 	"geo-ip-detection-headers": {
 		apiPath:                  "/api/config/v1/geographicRegions/ipDetectionHeaders",
+		deprecatedBy:             "builtin:rum.ip-mappings",
 		isSingleConfigurationApi: true,
 	},
 	"geo-ip-address-mappings": {
 		apiPath:                  "/api/config/v1/geographicRegions/ipAddressMappings",
+		deprecatedBy:             "builtin:rum.ip-determination",
 		isSingleConfigurationApi: true,
 	},
 }
