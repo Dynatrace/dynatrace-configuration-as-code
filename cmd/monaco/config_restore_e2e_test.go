07f6bcd8af9e19a326b3def770d2227ccf500362
UnseenWizzard <nicola.riedmann@dynatrace.com>
2023-01-31 15:40:06 +0100

    refactor(e2e): Reuse general integration test cleanup method for download/restore test
    Replace the copied cleanup method used in the config restore test with the generally used one,
    and slighlty modify how the test suffix is defined and used to match the fact that the general
    method pre-pends an underscore to the passed suffix, while config_restore viewed the underscore
    as part of the suffix so far.

--- cmd/monaco/config_restore_e2e_test.go
+++ cmd/monaco/config_restore_e2e_test.go
@@ -23,12 +23,8 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 	"testing"
 
-	"github.com/dynatrace-oss/dynatrace-monitoring-as-code/pkg/api"
-	"github.com/dynatrace-oss/dynatrace-monitoring-as-code/pkg/environment"
-	"github.com/dynatrace-oss/dynatrace-monitoring-as-code/pkg/rest"
 	"github.com/dynatrace-oss/dynatrace-monitoring-as-code/pkg/util"
 	"github.com/spf13/afero"
 	"gotest.tools/assert"
@@ -42,25 +38,26 @@ import (
 //Validation: Uploads the downloaded configs and checks for status code 0 as result
 //Cleanup: Deletes the configurations that were uploaded during validation
 
-//This version runs the test against 2 simple configs (alerting profiles and management zones)
+// This version runs the test against 2 simple configs (alerting profiles and management zones)
 func TestRestoreConfigsSimple(t *testing.T) {
 	initialConfigsFolder := "test-resources/integration-download-configs/"
 	envFile := initialConfigsFolder + "environments.yaml"
 	downloadFolder := "test-resources/download"
 	subsetOfConfigsToDownload := "alerting-profile,management-zone"
-	suffixTest := "_download1"
+	suffixTest := "download1"
 	testRestoreConfigs(t, initialConfigsFolder, downloadFolder, suffixTest, envFile, subsetOfConfigsToDownload)
 }
 
-//This version runs the test against the all_configs project, currently fails because of config dependencies
-// func TestRestoreConfigsFull(t *testing.T) {
-// 	initialConfigsFolder := "test-resources/integration-all-configs/"
-// 	envFile := initialConfigsFolder + "environments.yaml"
-// 	downloadFolder := "test-resources/download_all_configs"
-// 	subsetOfConfigsToDownload := "all" //value only for testing
-// 	suffixTest := "_dl1"
-// 	testRestoreConfigs(t, initialConfigsFolder, downloadFolder, suffixTest, envFile, subsetOfConfigsToDownload)
-// }
+// This version runs the test against the all_configs project, currently fails because of config dependencies
+//
+//	func TestRestoreConfigsFull(t *testing.T) {
+//		initialConfigsFolder := "test-resources/integration-all-configs/"
+//		envFile := initialConfigsFolder + "environments.yaml"
+//		downloadFolder := "test-resources/download_all_configs"
+//		subsetOfConfigsToDownload := "all" //value only for testing
+//		suffixTest := "dl1"
+//		testRestoreConfigs(t, initialConfigsFolder, downloadFolder, suffixTest, envFile, subsetOfConfigsToDownload)
+//	}
 func testRestoreConfigs(t *testing.T, initialConfigsFolder string, downloadFolder string, suffixTest string, envFile string, apisToDownload string) {
 	os.Setenv("NEW_CLI", "1")
 	fs := util.CreateTestFileSystem()
@@ -70,21 +67,21 @@ func testRestoreConfigs(t *testing.T, initialConfigsFolder string, downloadFolde
 		os.Setenv("NEW_CLI", "0")
 		return
 	}
-	err = execution_downloadConfigs(t, fs, downloadFolder, envFile, apisToDownload, suffixTest)
+	err = execution_downloadConfigs(t, fs, downloadFolder, envFile, apisToDownload)
 	if err != nil {
 		assert.NilError(t, err, "Error during download execution stage")
 		os.Setenv("NEW_CLI", "0")
 		return
 	}
-	cleanupEnvironmentConfigs(t, fs, envFile, suffixTest)
+	cleanupIntegrationTest(t, fs, envFile, suffixTest)
 	validation_uploadDownloadedConfigs(t, fs, downloadFolder, envFile)
-	cleanupEnvironmentConfigs(t, fs, envFile, suffixTest)
+	cleanupIntegrationTest(t, fs, envFile, suffixTest)
 	os.Setenv("NEW_CLI", "0")
 }
 
 func preparation_uploadConfigs(t *testing.T, fs afero.Fs, suffixTest string, configFolder string, envFile string) error {
 	util.Log.Info("BEGIN PREPARATION PROCESS")
-	suffix := getTimestamp() + suffixTest
+	suffix := getTimestamp() + "_" + suffixTest
 	transformers := []func(string) string{getTransformerFunc(suffix)}
 	err := util.RewriteConfigNames(configFolder, fs, transformers)
 	if err != nil {
@@ -101,7 +98,7 @@ func preparation_uploadConfigs(t *testing.T, fs afero.Fs, suffixTest string, con
 	return nil
 }
 func execution_downloadConfigs(t *testing.T, fs afero.Fs, downloadFolder string, envFile string,
-	apisToDownload string, suffixTest string) error {
+	apisToDownload string) error {
 	util.Log.Info("BEGIN DOWNLOAD PROCESS")
 	//Download
 	err := fs.MkdirAll(downloadFolder, 0777)
@@ -145,36 +142,4 @@ func validation_uploadDownloadedConfigs(t *testing.T, fs afero.Fs, downloadFolde
 		downloadFolder,
 	}, fs)
 	assert.Equal(t, statusCode, 0)
-}
-
-// Deletes all configs that end with "_suffix", where suffix == suffixTest+suffixTimestamp
-func cleanupEnvironmentConfigs(t *testing.T, fs afero.Fs, envFile, suffix string) {
-	util.Log.Info("BEGIN CLEANUP PROCESS")
-	environments, errs := environment.LoadEnvironmentList("", envFile, fs)
-	FailOnAnyError(errs, "loading of environments failed")
-
-	apis := api.NewApis()
-
-	for _, environment := range environments {
-
-		token, err := environment.GetToken()
-		assert.NilError(t, err)
-
-		client, err := rest.NewDynatraceClient(environment.GetEnvironmentUrl(), token)
-		assert.NilError(t, err)
-
-		for _, api := range apis {
-
-			values, err := client.List(api)
-			assert.NilError(t, err)
-
-			for _, value := range values {
-				// For the calculated-metrics-log API, the suffix is part of the ID, not name
-				if strings.HasSuffix(value.Name, suffix) || strings.HasSuffix(value.Id, suffix) {
-					util.Log.Info("Deleting %s (%s)", value.Name, api.GetId())
-					client.DeleteByName(api, value.Name)
-				}
-			}
-		}
-	}
 }
