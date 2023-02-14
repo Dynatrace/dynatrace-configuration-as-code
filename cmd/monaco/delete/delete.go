744f8c1b6a9b7d0f901622777fc183771c77835b
David Laubreiter <david.laubreiter@dynatrace.com>
2023-02-14 13:52:25 +0100

    feat: Delete allows to specify the deployment-group

--- cmd/monaco/delete/delete.go
+++ cmd/monaco/delete/delete.go
@@ -17,7 +17,7 @@ package delete
 import (
 	"errors"
 	"fmt"
-	"github.com/dynatrace-oss/dynatrace-monitoring-as-code/pkg/util/maps"
+	"github.com/dynatrace/dynatrace-configuration-as-code/pkg/util/maps"
 	"path/filepath"
 	"strings"
 
@@ -30,7 +30,7 @@ import (
 	"github.com/spf13/afero"
 )
 
-func Delete(fs afero.Fs, deploymentManifestPath string, deletePath string, environmentNames []string) error {
+func Delete(fs afero.Fs, deploymentManifestPath string, deletePath string, environmentNames []string, environmentGroup string) error {
 
 	deploymentManifestPath = filepath.Clean(deploymentManifestPath)
 	deploymentManifestPath, manifestErr := filepath.Abs(deploymentManifestPath)
@@ -64,9 +64,23 @@ func Delete(fs afero.Fs, deploymentManifestPath string, deletePath string, envir
 		return fmt.Errorf("encountered errors while parsing delete.yaml: %s", errs)
 	}
 
-	environments, err := manifest.Environments.FilterByNames(environmentNames)
-	if err != nil {
-		return fmt.Errorf("Failed to load environments: %w", err)
+	environments := manifest.Environments
+	if environmentGroup != "" {
+		environments = environments.FilterByGroup(environmentGroup)
+
+		if len(environments) == 0 {
+			return fmt.Errorf("no environments in group %q", environmentGroup)
+		} else {
+			log.Info("Environments loaded in group %q: %v", environmentGroup, maps.Keys(environments))
+		}
+	}
+
+	if len(environmentNames) > 0 {
+		var err error
+		environments, err = manifest.Environments.FilterByNames(environmentNames)
+		if err != nil {
+			return fmt.Errorf("failed to load environments: %w", err)
+		}
 	}
 
 	deleteErrors := deleteConfigs(maps.Values(environments), apis, entriesToDelete)
