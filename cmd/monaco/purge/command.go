6e4de3d3132c0dd052213e1d6e528919b85278ee
David Laubreiter <david.laubreiter@dynatrace.com>
2023-02-27 11:19:45 +0100

    refactor: Move purge-command

---
+++ cmd/monaco/purge/command.go
@@ -0,0 +1,67 @@
+/*
+ * @license
+ * Copyright 2023 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package purge
+
+import (
+	"fmt"
+	"github.com/dynatrace/dynatrace-configuration-as-code/cmd/monaco/cmdutils"
+	"github.com/dynatrace/dynatrace-configuration-as-code/cmd/monaco/runner/completion"
+	"github.com/dynatrace/dynatrace-configuration-as-code/internal/files"
+	"github.com/dynatrace/dynatrace-configuration-as-code/internal/log"
+	"github.com/spf13/afero"
+	"github.com/spf13/cobra"
+)
+
+func GetPurgeCommand(fs afero.Fs) (purgeCmd *cobra.Command) {
+
+	var environment []string
+	var manifestName string
+	var specificApis []string
+
+	purgeCmd = &cobra.Command{
+		Use:     "purge <manifest.yaml>",
+		Short:   "Delete ALL configurations from the environments defined in the manifest",
+		Example: "monaco purge manifest.yaml -e dev-environment",
+		Hidden:  true, // this command will not be suggested or shown in help
+		Args:    cobra.ExactArgs(1),
+		PreRun:  cmdutils.SilenceUsageCommand(),
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			manifestName = args[0]
+
+			if !files.IsYamlFileExtension(manifestName) {
+				err := fmt.Errorf("wrong format for manifest file! expected a .yaml file, but got %s", manifestName)
+				return err
+			}
+
+			return purge(fs, manifestName, environment, specificApis)
+		},
+		ValidArgsFunction: completion.PurgeCompletion,
+	}
+
+	purgeCmd.Flags().StringSliceVarP(&environment, "environment", "e", make([]string, 0), "Deletes configuration only for specified envs. If not set, delete will be executed on all environments defined in manifest.")
+	purgeCmd.Flags().StringSliceVarP(&specificApis, "api", "a", make([]string, 0), "One or more specific APIs to delete from (flag can be repeated or value defined as comma-separated list)")
+
+	if err := purgeCmd.RegisterFlagCompletionFunc("environment", completion.EnvironmentByArg0); err != nil {
+		log.Fatal("failed to setup CLI %v", err)
+	}
+	if err := purgeCmd.RegisterFlagCompletionFunc("api", completion.AllAvailableApis); err != nil {
+		log.Fatal("failed to setup CLI %v", err)
+	}
+
+	return purgeCmd
+}
--- cmd/monaco/purge/purge.go
+++ cmd/monaco/purge/purge.go
@@ -1,18 +1,20 @@
-// @license
-// Copyright 2021 Dynatrace LLC
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * @license
+ * Copyright 2023 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-package delete
+package purge
 
 import (
 	"errors"
@@ -21,13 +23,14 @@ import (
 	"github.com/dynatrace/dynatrace-configuration-as-code/internal/log"
 	"github.com/dynatrace/dynatrace-configuration-as-code/internal/maps"
 	"github.com/dynatrace/dynatrace-configuration-as-code/pkg/api"
+	"github.com/dynatrace/dynatrace-configuration-as-code/pkg/client"
 	"github.com/dynatrace/dynatrace-configuration-as-code/pkg/delete"
 	"github.com/dynatrace/dynatrace-configuration-as-code/pkg/manifest"
 	"github.com/spf13/afero"
 	"path/filepath"
 )
 
-func Purge(fs afero.Fs, deploymentManifestPath string, environmentNames []string, apiNames []string) error {
+func purge(fs afero.Fs, deploymentManifestPath string, environmentNames []string, apiNames []string) error {
 
 	deploymentManifestPath = filepath.Clean(deploymentManifestPath)
 	deploymentManifestPath, manifestErr := filepath.Abs(deploymentManifestPath)
@@ -78,7 +81,7 @@ func purgeConfigs(environments []manifest.EnvironmentDefinition, apis map[string
 }
 
 func purgeConfigsForEnvironment(env manifest.EnvironmentDefinition, apis map[string]api.Api) []error {
-	dynatraceClient, err := createClient(env, false)
+	dynatraceClient, err := createClient(env)
 
 	if err != nil {
 		return []error{
@@ -89,4 +92,19 @@ func purgeConfigsForEnvironment(env manifest.EnvironmentDefinition, apis map[str
 	log.Info("Deleting configs for environment `%s`", env.Name)
 
 	return delete.DeleteAllConfigs(dynatraceClient, apis)
+}
+
+func createClient(environment manifest.EnvironmentDefinition) (client.Client, error) {
+	token, err := environment.GetToken()
+
+	if err != nil {
+		return nil, err
+	}
+
+	url, err := environment.GetUrl()
+	if err != nil {
+		return nil, err
+	}
+
+	return client.NewDynatraceClient(url, token)
 }
