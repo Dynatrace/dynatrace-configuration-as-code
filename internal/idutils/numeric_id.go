96503038303d836fed7ad370516ed2fdf67a5342
UnseenWizzard <nicola.riedmann@dynatrace.com>
2023-04-05 17:07:50 +0200

    refactor(idutils): Simplify numeric ID generation
    Replace directly matching the Dynatrace implementation of decoding a UUID to
    a numeric identifier, by using standard binary.Varint decoding which implements
    variable-length quantity en-/decoding of integers and converting unsigned
    to signed using zig-zag decoding - which is what the internal implemenation
    does in more lines of code.
    
    Additionally the 'new' UUID number generation is simplified, as using the
    full 128bytes of the UUID is not necessary if we just want to convert its
    64 most significant bytes to an integer, which fit into a Uint64 directly.

--- internal/idutils/numeric_id.go
+++ internal/idutils/numeric_id.go
@@ -18,16 +18,16 @@ package idutils
 
 import (
 	"encoding/base64"
+	"encoding/binary"
 	"fmt"
 	"github.com/google/uuid"
-	"math/big"
 	"regexp"
 )
 
 var uuidRegex = regexp.MustCompile(".*?([0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}).*?")
 
 // GetNumericIDForObjectID parses the Settings Object ID of a Dynatrace Management Zone (only object with numeric IDs)
-// into a numeric identifier. To achieve this is replicates the en-/decoding logic used in Dynatrace as closely as possible.
+// into a numeric identifier. To achieve this it replicates the en-/decoding logic used in Dynatrace as closely as possible.
 func GetNumericIDForObjectID(objectID string) (int, error) {
 	decodedObjectID, err := base64.RawURLEncoding.DecodeString(objectID)
 	if err != nil {
@@ -42,85 +42,35 @@ func GetNumericIDForObjectID(objectID string) (int, error) {
 	uuidString := string(matches[1])
 
 	u, err := uuid.Parse(uuidString)
-	if err != nil {
+	if err != nil { // should never actually happen if the UUID regex matched
 		return 0, fmt.Errorf("failed to parse UUID %q: %w", uuidString, err)
 	}
 
 	if u.Variant() == uuid.RFC4122 && u.Version() == 4 {
-		return getLegacyNumericId(u), nil
-	} else {
-		return getNumericId(u), nil
+		return getLegacyNumericID(u)
 	}
 
+	return getNumericID(u), nil
 }
 
-func getNumericId(u uuid.UUID) int {
-	var val big.Int
-	val.SetBytes(u[:])             // turn the 16 UUID bytes into a 128bit number
-	val.SetBytes(val.Bytes()[0:8]) // keep the most significant 64bits (8byte)
-
-	numID := int(val.Uint64())
-
-	return numID
+// getNumericID implements the Dynatrace logic for transforming a "new" (variant RFC, version 4 (random)) UUID to a numeric ID
+// by converting the UUID's most significant (big-endian) bytes into an integer
+func getNumericID(u uuid.UUID) int {
+	return int(binary.BigEndian.Uint64(u[0:8]))
 }
 
-// getLegacyNumericId implements the Dynatrace logic for transforming a "legacy" random UUID to a numeric ID
-func getLegacyNumericId(u uuid.UUID) int {
-	uuidBytes := u[:] // work on the UUID's 16 bytes directly
+// getLegacyNumericID implements the Dynatrace logic for transforming a "legacy" UUID to a numeric ID
+// by taking specific bytes of the UUID and decoding them as a signed variable-length integer
+func getLegacyNumericID(u uuid.UUID) (int, error) {
 
-	var b [10]byte // create 10 byte array from which an 8 byte numeric ID will be created
+	var b []byte
+	b = u[0:6]                 // fill byte 0-5 with the UUID's most significant bytes (big-endian)
+	b = append(b, u[12:16]...) // fill byte 6-9 with the last 4 bytes of the UUID/ending "integer" of the UUID's least significant LSB
 
-	// fill byte 0-5 with the UUID's most significant bytes (big-endian)
-	b[0] = uuidBytes[0]
-	b[1] = uuidBytes[1]
-	b[2] = uuidBytes[2]
-	b[3] = uuidBytes[3]
-	b[4] = uuidBytes[4]
-	b[5] = uuidBytes[5]
-
-	// fill byte 6-9 with the last 4 bytes of the UUID/ending "integer" of the UUID's least significant LSB
-	b[6] = uuidBytes[12]
-	b[7] = uuidBytes[13]
-	b[8] = uuidBytes[14]
-	b[9] = uuidBytes[15]
-
-	numericId := byteToInt64(b)
-
-	numericId = zigZagDecode(numericId)
-
-	return int(numericId)
-}
-
-// byteToInt64 transforms the given byte array version of a numeric ID to an integer
-// using variable-length quantity encoding.
-// This implementation matches how Dynatrace transforms the byte array to a "long" directly.
-// Note the explicit use of int32 and int64 to match Java's int and long bit-lengths.
-// see: https://en.wikipedia.org/wiki/Variable-length_quantity
-// see:
-func byteToInt64(b [10]byte) int64 {
-	nextByte := int32(b[0])
-	if nextByte >= 0 && nextByte <= 128 {
-		return int64(nextByte)
-	}
-	res := int64(nextByte & 0x7F)
-	isContinuationBitSet := true
-	shift := 0
-	i := 0
-	for isContinuationBitSet {
-		i++
-		nextByte = int32(b[i])
-		isContinuationBitSet = (nextByte & 0x80) != 0
-		nextByte &= 0x7F
-		shift += 7
-		res |= int64(nextByte) << shift
+	numericId, n := binary.Varint(b) // decode bytes as signed VarInt
+	if n <= 0 {
+		return 0, fmt.Errorf("failed to decode variable-length integer. %d/%d bytes read correctly", -n, len(b))
 	}
 
-	return res
-}
-
-// zigZagDecode an int64
-// zig-zag decode shifts the input number by 1 including its sign bit, then applies an XOR mask
-// see: https://developers.google.com/protocol-buffers/docs/encoding
-func zigZagDecode(num int64) int64 {
-	return int64(uint64(num)>>1) ^ -(num & 1)
+	return int(numericId), nil
 }
