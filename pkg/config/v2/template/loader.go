a91dce094163ccb57939cdac10e0d8a0ebba9323
David Laubreiter <david.laubreiter@dynatrace.com>
2023-05-23 12:51:42 +0200

    fix: Allow backslashes in template paths
    When a project is created by a Windows user, the paths might contains backslashes (`\`) instead of slashes (`/`) for path-separation. This is not an issue if the project is deployed in Windows again, but if it's deployed on a Linux environment, for example in Linux-CI systems, the path could not be resolved.
    
    This bugfix allows backslashes in template paths, as a Windows-user might also manually create the path, and Monaco should handle it correctly.

--- pkg/config/v2/template/loader.go
+++ pkg/config/v2/template/loader.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"github.com/dynatrace/dynatrace-configuration-as-code/internal/log"
 	"path/filepath"
+	"strings"
 
 	"github.com/spf13/afero"
 )
@@ -104,7 +105,8 @@ var (
 // tries to load the file at the given path and turns it into a template.
 // the name of the template will be the sanitized path.
 func LoadTemplate(fs afero.Fs, path string) (Template, error) {
-	sanitizedPath := filepath.Clean(path)
+	log.Debug("Pre-Loading template for %s", path)
+	sanitizedPath := filepath.Clean(strings.ReplaceAll(path, `\`, `/`))
 
 	log.Debug("Loading template for %s", sanitizedPath)
 
