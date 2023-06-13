e3279b7aca2c4e2bf12cd485c8ddb45d4fc0ff6b
UnseenWizzard <nicola.riedmann@dynatrace.com>
2023-06-13 12:02:57 +0200

    feat(download): Extract UUIDs to YAML paramters as well
    With this any general identifiers that are not found in the downloaded configs,
    will be extracted to YAML parameters to simplify manual modification.

--- pkg/download/id_extraction/id_extraction_test.go
+++ pkg/download/id_extraction/id_extraction_test.go
@@ -88,6 +88,30 @@ func TestExtractIDsIntoYAML(t *testing.T) {
 			},
 		},
 		{
+			"finds and extracts UUID to parameter",
+			project.ConfigsPerType{
+				"test-type": []config.Config{
+					{
+						Template: template.CreateTemplateFromString("test-tmpl", `{ "key": "00b173f7-99ab-36e6-a365-170a7c42d364", "details": { "d1_key": "d1_val", "d2_key": "d2_val" } }`),
+						Parameters: config.Parameters{
+							"base-param": value.New("base-value"),
+						},
+					},
+				},
+			},
+			project.ConfigsPerType{
+				"test-type": []config.Config{
+					{
+						Template: template.CreateTemplateFromString("test-tmpl", `{ "key": "{{ .__EXTRACTED_ID_00b173f7_99ab_36e6_a365_170a7c42d364__ }}", "details": { "d1_key": "d1_val", "d2_key": "d2_val" } }`),
+						Parameters: config.Parameters{
+							"base-param": value.New("base-value"),
+							"__EXTRACTED_ID_00b173f7_99ab_36e6_a365_170a7c42d364__": value.New("00b173f7-99ab-36e6-a365-170a7c42d364"),
+						},
+					},
+				},
+			},
+		},
+		{
 			"finds and extracts several meIDs to parameters",
 			project.ConfigsPerType{
 				"test-type": []config.Config{
@@ -137,6 +161,30 @@ func TestExtractIDsIntoYAML(t *testing.T) {
 			},
 		},
 		{
+			"creates only a single parameter for repeated UUIDs",
+			project.ConfigsPerType{
+				"test-type": []config.Config{
+					{
+						Template: template.CreateTemplateFromString("test-tmpl", `{ "key": "00b173f7-99ab-36e6-a365-170a7c42d364", "details": { "d1_key": "AWS_RELATIONAL_DATABASE_SERVICE", "d2_key": "00b173f7-99ab-36e6-a365-170a7c42d364" } }`),
+						Parameters: config.Parameters{
+							"base-param": value.New("base-value"),
+						},
+					},
+				},
+			},
+			project.ConfigsPerType{
+				"test-type": []config.Config{
+					{
+						Template: template.CreateTemplateFromString("test-tmpl", `{ "key": "{{ .__EXTRACTED_ID_00b173f7_99ab_36e6_a365_170a7c42d364__ }}", "details": { "d1_key": "AWS_RELATIONAL_DATABASE_SERVICE", "d2_key": "{{ .__EXTRACTED_ID_00b173f7_99ab_36e6_a365_170a7c42d364__ }}" } }`),
+						Parameters: config.Parameters{
+							"base-param": value.New("base-value"),
+							"__EXTRACTED_ID_00b173f7_99ab_36e6_a365_170a7c42d364__": value.New("00b173f7-99ab-36e6-a365-170a7c42d364"),
+						},
+					},
+				},
+			},
+		},
+		{
 			"correctly extracts an updates all configs",
 			project.ConfigsPerType{
 				"test-type": []config.Config{
@@ -162,6 +210,12 @@ func TestExtractIDsIntoYAML(t *testing.T) {
 					},
 					{
 						Template: template.CreateTemplateFromString("test-tmpl", `{ "key": "SYNTHETIC_LOCATION-4242424242424242", "details": { "d1_key": "d1_val", "d2_key": "d2_val" } }`),
+						Parameters: config.Parameters{
+							"base-param": value.New("base-value"),
+						},
+					},
+					{
+						Template: template.CreateTemplateFromString("test-tmpl", `{ "key": "SYNTHETIC_LOCATION-4242424242424242", "details": { "d1_key": "00b173f7-99ab-36e6-a365-170a7c42d364", "d2_key": "d2_val" } }`),
 						Parameters: config.Parameters{
 							"base-param": value.New("base-value"),
 						},
@@ -198,6 +252,14 @@ func TestExtractIDsIntoYAML(t *testing.T) {
 						Parameters: config.Parameters{
 							"base-param": value.New("base-value"),
 							"__EXTRACTED_ID_SYNTHETIC_LOCATION_4242424242424242__": value.New("SYNTHETIC_LOCATION-4242424242424242"),
+						},
+					},
+					{
+						Template: template.CreateTemplateFromString("test-tmpl", `{ "key": "{{ .__EXTRACTED_ID_SYNTHETIC_LOCATION_4242424242424242__ }}", "details": { "d1_key": "{{ .__EXTRACTED_ID_00b173f7_99ab_36e6_a365_170a7c42d364__ }}", "d2_key": "d2_val" } }`),
+						Parameters: config.Parameters{
+							"base-param": value.New("base-value"),
+							"__EXTRACTED_ID_SYNTHETIC_LOCATION_4242424242424242__":  value.New("SYNTHETIC_LOCATION-4242424242424242"),
+							"__EXTRACTED_ID_00b173f7_99ab_36e6_a365_170a7c42d364__": value.New("00b173f7-99ab-36e6-a365-170a7c42d364"),
 						},
 					},
 				},
@@ -242,7 +304,7 @@ func TestExtractedTemplatesRenderCorrectly(t *testing.T) {
 				},
 			},
 			{
-				Template: template.CreateTemplateFromString("test-tmpl", `{ "key": "SYNTHETIC_LOCATION-4242424242424242", "details": { "d1_key": "HOST_GROUP-1234567890123456", "d2_key": "d2_val" } }`),
+				Template: template.CreateTemplateFromString("test-tmpl", `{ "key": "SYNTHETIC_LOCATION-4242424242424242", "details": { "d1_key": "HOST_GROUP-1234567890123456", "d2_key": "00b173f7-99ab-36e6-a365-170a7c42d364" } }`),
 				Parameters: config.Parameters{
 					"base-param": value.New("base-value"),
 				},
