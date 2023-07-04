4194ad0596ff8cba38f04b4297a901fa069faaaf
David Laubreiter <david.laubreiter@dynatrace.com>
2023-07-04 15:23:12 +0200

    ci: Add jenkins pipeline for nightly hardening tests
    This new pipeline executes all e2e tests on a Dynatrace hardening environment.

---
+++ .ci/nightly.groovy
@@ -0,0 +1,81 @@
+pipeline {
+    agent {
+        kubernetes {
+            label 'ca-jenkins-agent'
+            cloud 'linux-amd64'
+            namespace 'keptn-jenkins-slaves-ni'
+            nodeSelector 'beta.kubernetes.io/arch=amd64,beta.kubernetes.io/os=linux'
+            instanceCap '2'
+            idleMinutes '2'
+            yamlFile '.ci/jenkins_agents/ca-jenkins-agent.yaml'
+        }
+    }
+
+    stages {
+
+        stage("🌎 Integration test") {
+            steps {
+                executeWithSecrets(cmd: 'make integration-test')
+            }
+        }
+
+        stage("🧓 Integration test (legacy)") {
+            steps {
+                executeWithSecrets(cmd: 'make integration-test-v1')
+            }
+        }
+
+        stage("📥/📤 Download/Restore test") {
+            steps {
+                executeWithSecrets(cmd: 'make download-restore-test')
+            }
+        }
+
+        stage("🌙 Nightly Tests") {
+            steps {
+                executeWithSecrets(cmd: 'make nightly-test')
+            }
+        }
+
+        stage("🧹 Cleanup") {
+            steps {
+                executeWithSecrets(cmd: 'make clean-environments')
+            }
+        }
+    }
+
+
+    post {
+        failure {
+            emailext recipientProviders: [culprits()], subject: '$DEFAULT_SUBJECT', mimeType: 'text/html', body: '$DEFAULT_CONTENT'
+        }
+        unstable {
+            emailext recipientProviders: [culprits()], subject: '$DEFAULT_SUBJECT', mimeType: 'text/html', body: '$DEFAULT_CONTENT'
+        }
+    }
+}
+
+def executeWithSecrets(Map args = [cmd: null]) {
+    withVault(vaultSecrets:
+        [[
+             path        : "keptn-jenkins/monaco/integration-tests/hardening",
+             secretValues: [
+                 [envVar: 'OAUTH_CLIENT_ID', vaultKey: 'OAUTH_CLIENT_ID', isRequired: true],
+                 [envVar: 'OAUTH_CLIENT_SECRET', vaultKey: 'OAUTH_CLIENT_SECRET', isRequired: true],
+                 [envVar: 'OAUTH_TOKEN_ENDPOINT', vaultKey: 'OAUTH_TOKEN_ENDPOINT', isRequired: true],
+
+                 [envVar: 'URL_ENVIRONMENT_1', vaultKey: 'URL_ENVIRONMENT_1', isRequired: true],
+                 [envVar: 'TOKEN_ENVIRONMENT_1', vaultKey: 'TOKEN_ENVIRONMENT_1', isRequired: true],
+                 [envVar: 'PLATFORM_URL_ENVIRONMENT_1', vaultKey: 'PLATFORM_URL_ENVIRONMENT_1', isRequired: true],
+
+                 [envVar: 'URL_ENVIRONMENT_2', vaultKey: 'URL_ENVIRONMENT_2', isRequired: true],
+                 [envVar: 'TOKEN_ENVIRONMENT_2', vaultKey: 'TOKEN_ENVIRONMENT_2', isRequired: true],
+                 [envVar: 'PLATFORM_URL_ENVIRONMENT_2', vaultKey: 'PLATFORM_URL_ENVIRONMENT_2', isRequired: true],
+             ]
+         ]]) {
+        catchError(buildResult: 'FAILURE', stageResult: 'FAILURE') {
+            sh(script: args.cmd)
+        }
+    }
+
+}
