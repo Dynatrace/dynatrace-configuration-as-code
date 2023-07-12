671fa4e41239b0aa8e322f7d1f703b718913eb1e
UnseenWizzard <nicola.riedmann@dynatrace.com>
2023-07-12 16:10:12 +0200

    feat: Add CLI command for generating dependency graphs

---
+++ cmd/monaco/dependencygraph/dependencygraph.go
@@ -0,0 +1,85 @@
+// @license
+// Copyright 2021 Dynatrace LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dependencygraph
+
+import (
+	"fmt"
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/internal/errutils"
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/internal/log"
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/api"
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/config"
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/graph"
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/manifest"
+	project "github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/project/v2"
+	"github.com/spf13/afero"
+	"path/filepath"
+)
+
+func writeGraphFiles(fs afero.Fs, manifestPath string, environmentNames []string, environmentGroups []string, outputFolder string) error {
+
+	m, errs := manifest.LoadManifest(&manifest.LoaderContext{
+		Fs:           fs,
+		ManifestPath: manifestPath,
+		Environments: environmentNames,
+		Groups:       environmentGroups,
+	})
+	if len(errs) > 0 {
+		errutils.PrintErrors(errs)
+		return fmt.Errorf("failed to load manifest %q", manifestPath)
+	}
+
+	projects, errs := project.LoadProjects(fs, project.ProjectLoaderContext{
+		KnownApis:       api.NewAPIs().GetApiNameLookup(),
+		WorkingDir:      filepath.Dir(manifestPath),
+		Manifest:        m,
+		ParametersSerde: config.DefaultParameterParsers,
+	})
+
+	if len(errs) > 0 {
+		errutils.PrintErrors(errs)
+		return fmt.Errorf("failed to load projects")
+	}
+
+	graphs := graph.New(projects, m.Environments.Names())
+
+	folderPath, err := filepath.Abs(outputFolder)
+	if err != nil {
+		return fmt.Errorf("failed to access output path: %q: %w", outputFolder, err)
+	}
+
+	if outputFolder != "" {
+		if exits, _ := afero.Exists(fs, folderPath); !exits {
+			err = fs.Mkdir(folderPath, 0777)
+			if err != nil {
+				return fmt.Errorf("failed to create output folder: %q", folderPath)
+			}
+		}
+	}
+
+	for _, e := range m.Environments.Names() {
+		b, err := graphs.EncodeToDOT(e)
+		if err != nil {
+			return fmt.Errorf("failed to encode dependency graph to DOT for environment %q: %w", e, err)
+		}
+		file := filepath.Join(folderPath, fmt.Sprintf("dependency_graph_%s.dot", e))
+		err = afero.WriteFile(fs, file, b, 0666)
+		if err != nil {
+			return fmt.Errorf("failed to create dependency graph file %q: %w", file, err)
+		}
+		log.Info("Dependency graph for environment %q written to %q", e, file)
+	}
+
+	return nil
+}
