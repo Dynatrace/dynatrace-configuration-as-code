82f95228e8c8a11882e992003422e08b18640a98
UnseenWizzard <nicola.riedmann@dynatrace.com>
2023-09-14 13:21:02 +0200

    test(e2e): Assert that deployment errors are presented to users as expected
    As only a very small handful of tests broke while refactoring the handling of errors
    and their logging in deploy, a test is added to at least give some level of coverage
    when messing with how errors are presented to users in the future.
    
    Like other "produces_user_errors" tests, this is added as an E2E test, to ensure the
    actual CLI as a whole product presents the information we want, instead of asserting
    this on a lower package unit test level. E.g. deploy might still log correctly if you
    give it the right logger, but the CLI might not if something in the setup code breaks
    - which a CLI E2E test will still cover.
    
    This test tries to strike a balance of testing specific enough messages to be sure
    errors are still presented with the necessarry information, while not asserting
    exact strings - that will hopefully allow small changes without constantly breaking
    this test.

---
+++ cmd/monaco/integrationtest/v2/invalid_payloads_produce_user_errors_e2e_test.go
@@ -0,0 +1,55 @@
+//go:build integration
+// +build integration
+
+/**
+ * @license
+ * Copyright 2020 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2
+
+import (
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/internal/testutils"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/cmd/monaco/runner"
+	"github.com/spf13/afero"
+)
+
+func TestAPIErrorsAreReported(t *testing.T) {
+	configFolder := "test-resources/configs-with-invalid-payload/"
+	manifest := configFolder + "manifest.yaml"
+
+	RunIntegrationWithCleanup(t, configFolder, manifest, "", "InvalidJSON", func(fs afero.Fs, _ TestContext) {
+
+		logOutput := strings.Builder{}
+		cmd := runner.BuildCliWithLogSpy(testutils.CreateTestFileSystem(), &logOutput)
+		cmd.SetArgs([]string{"deploy", "--verbose", manifest, "--continue-on-error"})
+		err := cmd.Execute()
+
+		assert.ErrorContains(t, err, "Deployment failed")
+		assert.ErrorContains(t, err, "2 environment(s)")
+		assert.ErrorContains(t, err, "classic_env")
+		assert.ErrorContains(t, err, "platform_env")
+		assert.ErrorContains(t, err, "2 deployment errors")
+
+		runLog := strings.ToLower(logOutput.String())
+		assert.Regexp(t, ".*?error.*?invalid-config-api-with-settings-payload.*?deployment failed - dynatrace api rejected http request.*?", runLog)
+		assert.Regexp(t, ".*?error.*?tags.auto-tagging:invalid-setting-with-config-api-payload.*?deployment failed - dynatrace api rejected http request.*?", runLog)
+		assert.Contains(t, runLog, "deployment failed for environment \"classic_env\"")
+		assert.Contains(t, runLog, "deployment failed for environment \"platform_env\"")
+	})
+}
