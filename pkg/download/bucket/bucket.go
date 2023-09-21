2e3a107d2b0618b5c9064f00f4cd21e53325db95
Jure Skelin <jure.skelin@dynatrace.com>
2023-09-21 14:58:55 +0200

    fix: PR

--- pkg/download/bucket/bucket.go
+++ pkg/download/bucket/bucket.go
@@ -47,7 +47,7 @@ func NewDownloader(client BucketClient) *Downloader {
 		client: client,
 	}
 }
-func (d *Downloader) Download(projectName string, _ ...config.BucketType) (v2.ConfigsPerType, error) {
+func (d *Downloader) Download(projectName string, _ ...config.BucketType) (v2.ConfigsPerType, error) { // error in return is just to complain to interface
 	result := make(v2.ConfigsPerType)
 	response, err := d.client.List(context.TODO())
 	if err != nil {
@@ -64,80 +64,6 @@ func (d *Downloader) Download(projectName string, _ ...config.BucketType) (v2.Co
 	result["bucket"] = configs
 	return result, nil
 }
-
-//func (d *Downloader) convertAllObjects(projectName string, objects [][]byte) []config.Config {
-//	result := make([]config.Config, 0, len(objects))
-//	for _, o := range objects {
-//		var bucketName struct {
-//			BucketName  string `json:"bucketName"`
-//			DisplayName string `json:"displayName"`
-//		}
-//		if err := json.Unmarshal(o, &bucketName); err != nil {
-//			return result
-//		}
-//
-//		// exclude builtin bucket names
-//		if strings.HasPrefix(bucketName.BucketName, "dt_") || strings.HasPrefix(bucketName.BucketName, "default_") {
-//			continue
-//		}
-//
-//		// construct config object with generated config ID
-//		configID := idutils.GenerateUUIDFromString(bucketName.BucketName)
-//		configCoord := coordinate.Coordinate{
-//			Project:  projectName,
-//			Type:     "bucket",
-//			ConfigId: configID,
-//		}
-//
-//		params := map[string]parameter.Parameter{}
-//		{
-//			var s string
-//			var err error
-//
-//			s, err = getValueForAttribute(o, "bucketName")
-//			if err != nil {
-//				log.WithFields(field.Coordinate(coordinate.Coordinate{Project: projectName, Type: "bucket", ConfigId: configID}), field.Error(err)).Warn("Failed to get configuration for %v (%s): %v", configID, "bucket", err)
-//				continue
-//			}
-//			params[config.NameParameter] = &value.ValueParameter{Value: s}
-//			o, err = replaceAttributeWith(o, "bucketName", config.NameParameter)
-//			if err != nil {
-//				log.WithFields(field.Coordinate(coordinate.Coordinate{Project: projectName, Type: "bucket", ConfigId: configID}), field.Error(err)).Warn("Failed to get configuration for %v (%s): %v", configID, "bucket", err)
-//				continue
-//			}
-//
-//			s, err = getValueForAttribute(o, "displayName")
-//			if err != nil {
-//				log.WithFields(field.Coordinate(coordinate.Coordinate{Project: projectName, Type: "bucket", ConfigId: configID}), field.Error(err)).Warn("Failed to get configuration for %v (%s): %v", configID, "bucket", err)
-//				continue
-//			}
-//			if s != "" {
-//				params["displayName"] = &value.ValueParameter{Value: s}
-//				o, err = replaceAttributeWith(o, "displayName", "displayName")
-//				if err != nil {
-//					log.WithFields(field.Coordinate(coordinate.Coordinate{Project: projectName, Type: "bucket", ConfigId: configID}), field.Error(err)).Warn("Failed to get configuration for %v (%s): %v", configID, "bucket", err)
-//					continue
-//				}
-//			}
-//
-//		}
-//
-//		originObjectID := fmt.Sprintf("%s_%s", configCoord.Project, configCoord.ConfigId)
-//
-//		c := config.Config{
-//			Template:       template.NewDownloadTemplate(configID, configID, string(jsonutils.MarshalIndent(o))),
-//			Parameters:     params,
-//			Coordinate:     configCoord,
-//			Type:           config.BucketType{},
-//			OriginObjectId: originObjectID,
-//		}
-//		result = append(result, c)
-//	}
-//
-//	log.Info("downloaded %d configurations for GRAIL bucket", len(result))
-//
-//	return result
-//}
 
 func (d *Downloader) convertAllObjects(projectName string, objects [][]byte) []config.Config {
 	result := make([]config.Config, 0, len(objects))
