87f8e055d50242ca6409c9171ff6151254294ab5
UnseenWizzard <nicola.riedmann@dynatrace.com>
2023-11-10 14:46:54 +0100

    feat(account): Allow writing Account Resources to disk
    Introduce a new writer package to pkg/persistence/account which transforms
    data from in-memory representation to the YAML persistence structs, and writes
    them to 'users.yaml', 'policies.yaml' and 'groups.yaml' files in a given folder.

---
+++ pkg/persistence/account/integration_test.go
@@ -0,0 +1,131 @@
+/*
+ * @license
+ * Copyright 2023 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package account_test
+
+import (
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/account"
+	persistence "github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/persistence/account"
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/persistence/account/loader"
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/persistence/account/writer"
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+	"path/filepath"
+	"testing"
+)
+
+func TestLoadAndReWriteAccountResources(t *testing.T) {
+	testResources := "loader/test-resources/multi"
+	fs := afero.NewCopyOnWriteFs(afero.NewOsFs(), afero.NewMemMapFs())
+
+	// LOAD RESOURCES FROM DISK
+	resources, err := loader.Load(fs, testResources)
+	assert.NoError(t, err)
+
+	assert.NotEmpty(t, resources.Groups)
+	assert.NotEmpty(t, resources.Policies)
+	assert.NotEmpty(t, resources.Users)
+
+	// TRANSFORM TO IN_MEMORY
+	//TODO: remove when loader returns in memory model
+	inMemResources := toInMemRepresentation(*resources)
+
+	// WRITE IN-MEMORY REPRESENTATION TO DISK
+	c := writer.Context{
+		Fs:            fs,
+		OutputFolder:  "test-folder",
+		ProjectFolder: "test-project",
+	}
+	err = writer.WriteAccountResources(c, inMemResources)
+	assert.NoError(t, err)
+
+	// ASSERT FILES WRITTEN AS EXPECTED
+	expectedOutputFolder, err := filepath.Abs(filepath.Join(c.OutputFolder, c.ProjectFolder))
+	assert.NoError(t, err)
+	assertFileExists(t, c.Fs, filepath.Join(expectedOutputFolder, "users.yaml"))
+	assertFileExists(t, c.Fs, filepath.Join(expectedOutputFolder, "groups.yaml"))
+	assertFileExists(t, c.Fs, filepath.Join(expectedOutputFolder, "policies.yaml"))
+
+	// ASSERT WRITTEN FILES MATCH ORIGINALS AFTER LOADING THEM FROM DISK
+	writtenResources, err := loader.Load(fs, expectedOutputFolder)
+	assert.NoError(t, err)
+	assert.Equal(t, resources.Groups, writtenResources.Groups)
+	assert.Equal(t, resources.Policies, writtenResources.Policies)
+	assert.Equal(t, resources.Users, writtenResources.Users)
+}
+
+func toInMemRepresentation(resources persistence.AMResources) account.Resources {
+	inMemResources := account.Resources{
+		Policies: make(map[account.PolicyId]account.Policy),
+		Groups:   make(map[account.GroupId]account.Group),
+		Users:    make(map[account.UserId]account.User),
+	}
+	for id, v := range resources.Policies {
+		inMemResources.Policies[id] = account.Policy{
+			ID:          v.ID,
+			Name:        v.Name,
+			Level:       v.Level,
+			Description: v.Description,
+			Policy:      v.Policy,
+		}
+	}
+	for id, v := range resources.Groups {
+		var acc *account.Account
+		if v.Account != nil {
+			acc = &account.Account{
+				Permissions: v.Account.Permissions,
+				Policies:    v.Account.Policies,
+			}
+		}
+		env := make([]account.Environment, len(v.Environment))
+		for i, e := range v.Environment {
+			env[i] = account.Environment{
+				Name:        e.Name,
+				Permissions: e.Permissions,
+				Policies:    e.Policies,
+			}
+		}
+		mz := make([]account.ManagementZone, len(v.ManagementZone))
+		for i, m := range v.ManagementZone {
+			mz[i] = account.ManagementZone{
+				Environment:    m.Environment,
+				ManagementZone: m.ManagementZone,
+				Permissions:    m.Permissions,
+			}
+		}
+		inMemResources.Groups[id] = account.Group{
+			ID:             v.ID,
+			Name:           v.Name,
+			Description:    v.Description,
+			Account:        acc,
+			Environment:    env,
+			ManagementZone: mz,
+		}
+	}
+	for id, v := range resources.Users {
+		inMemResources.Users[id] = account.User{
+			Email:  v.Email,
+			Groups: v.Groups,
+		}
+	}
+	return inMemResources
+}
+
+func assertFileExists(t *testing.T, fs afero.Fs, path string) {
+	exists, err := afero.Exists(fs, path)
+	assert.NoError(t, err)
+	assert.True(t, exists, "expected file to exist %v", path)
+}
---
+++ pkg/persistence/account/writer/writer_test.go
@@ -0,0 +1,297 @@
+/*
+ * @license
+ * Copyright 2023 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package writer_test
+
+import (
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/account"
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/persistence/account/writer"
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+	"path/filepath"
+	"testing"
+)
+
+func TestWriteAccountResources(t *testing.T) {
+	type want struct {
+		groups   string
+		users    string
+		policies string
+	}
+	tests := []struct {
+		name           string
+		givenResources account.Resources
+		wantPersisted  want
+	}{
+		{
+			"only users",
+			account.Resources{
+				Users: map[account.UserId]account.User{
+					"monaco@dynatrace.com": account.User{
+						Email: "monaco@dynatrace.com",
+						Groups: []any{
+							account.Reference{
+								Type: "reference",
+								Id:   "my-group",
+							},
+							"Log viewer",
+						},
+					},
+				},
+			},
+			want{
+				users: `users:
+- email: monaco@dynatrace.com
+  groups:
+  - type: reference
+    id: my-group
+  - Log viewer
+`,
+			},
+		},
+		{
+			"only groups",
+			account.Resources{
+				Groups: map[account.GroupId]account.Group{
+					"my-group": {
+						ID:          "my-group",
+						Name:        "My Group",
+						Description: "This is my group",
+						Account: &account.Account{
+							Permissions: []any{"View my Group Stuff"},
+							Policies:    []any{"Request my Group Stuff"},
+						},
+						Environment: []account.Environment{
+							{
+								Name:        "myenv123",
+								Permissions: []any{"View environment"},
+								Policies: []any{
+									"View environment",
+									account.Reference{
+										Type: "reference",
+										Id:   "my-policy",
+									},
+								},
+							},
+						},
+						ManagementZone: []account.ManagementZone{
+							{
+								Environment:    "myenv123",
+								ManagementZone: "My MZone",
+								Permissions:    []any{"Do Stuff"},
+							},
+						},
+					},
+				},
+			},
+			want{
+				groups: `groups:
+- id: my-group
+  name: My Group
+  description: This is my group
+  account:
+    permissions:
+    - View my Group Stuff
+    policies:
+    - Request my Group Stuff
+  environment:
+  - name: myenv123
+    permissions:
+    - View environment
+    policies:
+    - View environment
+    - type: reference
+      id: my-policy
+  managementZone:
+  - environment: myenv123
+    managementZone: My MZone
+    permissions:
+    - Do Stuff
+`,
+			},
+		},
+		{
+			"only policies",
+			account.Resources{
+				Policies: map[account.PolicyId]account.Policy{
+					"my-policy": {
+						ID:          "my-policy",
+						Name:        "My Policy",
+						Level:       account.PolicyLevelAccount{Type: "account"},
+						Description: "This is my policy. There's many like it, but this one is mine.",
+						Policy:      "ALLOW a:b:c;",
+					},
+				},
+			},
+			want{policies: `policies:
+- id: my-policy
+  name: My Policy
+  level:
+    type: account
+  description: This is my policy. There's many like it, but this one is mine.
+  policy: ALLOW a:b:c;
+`,
+			},
+		},
+		{
+			"full resources",
+			account.Resources{
+				Users: map[account.UserId]account.User{
+					"monaco@dynatrace.com": account.User{
+						Email: "monaco@dynatrace.com",
+						Groups: []any{
+							account.Reference{
+								Type: "reference",
+								Id:   "my-group",
+							},
+							"Log viewer",
+						},
+					},
+				},
+				Groups: map[account.GroupId]account.Group{
+					"my-group": {
+						ID:          "my-group",
+						Name:        "My Group",
+						Description: "This is my group",
+						Account: &account.Account{
+							Permissions: []any{"View my Group Stuff"},
+							Policies:    []any{"Request my Group Stuff"},
+						},
+						Environment: []account.Environment{
+							{
+								Name:        "myenv123",
+								Permissions: []any{"View environment"},
+								Policies: []any{
+									"View environment",
+									account.Reference{
+										Type: "reference",
+										Id:   "my-policy",
+									},
+								},
+							},
+						},
+						ManagementZone: []account.ManagementZone{
+							{
+								Environment:    "myenv123",
+								ManagementZone: "My MZone",
+								Permissions:    []any{"Do Stuff"},
+							},
+						},
+					},
+				},
+				Policies: map[account.PolicyId]account.Policy{
+					"my-policy": {
+						ID:          "my-policy",
+						Name:        "My Policy",
+						Level:       account.PolicyLevelAccount{Type: "account"},
+						Description: "This is my policy. There's many like it, but this one is mine.",
+						Policy:      "ALLOW a:b:c;",
+					},
+				},
+			},
+			want{
+				users: `users:
+- email: monaco@dynatrace.com
+  groups:
+  - type: reference
+    id: my-group
+  - Log viewer
+`,
+				groups: `groups:
+- id: my-group
+  name: My Group
+  description: This is my group
+  account:
+    permissions:
+    - View my Group Stuff
+    policies:
+    - Request my Group Stuff
+  environment:
+  - name: myenv123
+    permissions:
+    - View environment
+    policies:
+    - View environment
+    - type: reference
+      id: my-policy
+  managementZone:
+  - environment: myenv123
+    managementZone: My MZone
+    permissions:
+    - Do Stuff
+`,
+				policies: `policies:
+- id: my-policy
+  name: My Policy
+  level:
+    type: account
+  description: This is my policy. There's many like it, but this one is mine.
+  policy: ALLOW a:b:c;
+`,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := writer.Context{
+				Fs:            afero.NewMemMapFs(),
+				OutputFolder:  "test/",
+				ProjectFolder: "project/",
+			}
+			err := writer.WriteAccountResources(c, tt.givenResources)
+			assert.NoError(t, err)
+
+			expectedFolder, _ := filepath.Abs(c.OutputFolder)
+
+			users := filepath.Join(expectedFolder, c.ProjectFolder, "users.yaml")
+			if tt.wantPersisted.users == "" {
+				exists, err := afero.Exists(c.Fs, users)
+				assert.NoError(t, err)
+				assert.False(t, exists, "expected no users file to be created")
+			} else {
+				assertFile(t, c.Fs, users, tt.wantPersisted.users)
+			}
+
+			groups := filepath.Join(expectedFolder, c.ProjectFolder, "groups.yaml")
+			if tt.wantPersisted.groups == "" {
+				exists, err := afero.Exists(c.Fs, groups)
+				assert.NoError(t, err)
+				assert.False(t, exists, "expected no groups file to be created")
+			} else {
+				assertFile(t, c.Fs, groups, tt.wantPersisted.groups)
+			}
+
+			policies := filepath.Join(expectedFolder, c.ProjectFolder, "policies.yaml")
+			if tt.wantPersisted.policies == "" {
+				exists, err := afero.Exists(c.Fs, policies)
+				assert.NoError(t, err)
+				assert.False(t, exists, "expected no policies file to be created")
+			} else {
+				assertFile(t, c.Fs, policies, tt.wantPersisted.policies)
+			}
+
+		})
+	}
+}
+
+func assertFile(t *testing.T, fs afero.Fs, expectedPath, expectedContent string) {
+	exists, err := afero.Exists(fs, expectedPath)
+	assert.True(t, exists)
+	assert.NoError(t, err)
+	got, err := afero.ReadFile(fs, expectedPath)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedContent, string(got))
+}
