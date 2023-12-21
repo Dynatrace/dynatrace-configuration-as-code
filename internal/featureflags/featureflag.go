c0893c38d2d085c3517d7df0cd162489ee89d2b3
Jure Skelin <jure.skelin@dynatrace.com>
2023-12-21 15:02:11 +0100

    fix: enable featureflags.New only for testing

--- internal/featureflags/featureflag.go
+++ internal/featureflags/featureflag.go
@@ -36,16 +36,6 @@ type FeatureFlag struct {
 	defaultEnabled bool
 }
 
-// New creates a new FeatureFlag
-// envName is the environment variable the feature flag is loading the values from when evaluated
-// defaultEnabled defines whether the feature flag is enabled or not by default
-func New(envName string, defaultEnabled bool) FeatureFlag {
-	return FeatureFlag{
-		envName:        envName,
-		defaultEnabled: defaultEnabled,
-	}
-}
-
 // Enabled evaluates the feature flag.
 // Feature flags are considered to be "enabled" if their resp. environment variable
 // is set to 1, t, T, TRUE, true or True.
