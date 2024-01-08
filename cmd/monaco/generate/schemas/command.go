70133a6e4f2cfee799dbce455cd42ee62fde78d1
Nico Riedmann <UnseenWizzard@users.noreply.github.com>
2024-01-08 13:11:46 +0100

    docs: Improve wording of generate schema command description
    Co-authored-by: David Laubreiter <3740651+Laubi@users.noreply.github.com>

--- cmd/monaco/generate/schemas/command.go
+++ cmd/monaco/generate/schemas/command.go
@@ -28,7 +28,7 @@ func Command(fs afero.Fs) (cmd *cobra.Command) {
 
 	cmd = &cobra.Command{
 		Use:     "schemas",
-		Short:   "Generate JSON schemas for YAML files like manifests, configuration files or delete files.",
+		Short:   "Generate JSON schemas for YAML files like manifest, configuration, or delete files.",
 		Example: "monaco generate schemas -o output-folder",
 		Args:    cobra.NoArgs,
 		PreRun:  cmdutils.SilenceUsageCommand(),
