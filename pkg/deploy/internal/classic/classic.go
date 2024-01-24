946a870a72f84b2cd6efeaf5f159c8e94b446343
David Laubreiter <3740651+Laubi@users.noreply.github.com>
2024-01-24 16:26:22 +0100

    fix: Replace SubPath method by SubPath Property as it broke deployment (#1345)
    fix: Replace SubPath method by SubPath Property
    
    The SubPath method broke the deployment as the former method checked whether the url contained the {SCOPE} sub path.
    This leads (and led) to a problem when we depended in a later step on the subpath check, and at that time, the proper url without the {SCOPE} was already used instead.

--- pkg/deploy/internal/classic/classic.go
+++ pkg/deploy/internal/classic/classic.go
@@ -41,7 +41,7 @@ func Deploy(ctx context.Context, configClient dtclient.ConfigClient, apis api.AP
 		return entities.ResolvedEntity{}, fmt.Errorf("unknown api `%s`. this is most likely a bug", t.Api)
 	}
 
-	if apiToDeploy.IsSubPathAPI() {
+	if apiToDeploy.SubPathAPI {
 		scope, err := extract.Scope(properties)
 		if err != nil {
 			return entities.ResolvedEntity{}, fmt.Errorf("failed to extract scope for config %q", conf.Type.ID())
