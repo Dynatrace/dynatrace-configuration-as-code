ae14a5bfe5e46a5a8838b1086110db202b55ebcc
Jure Skelin <jure.skelin@dynatrace.com>
2024-03-22 08:16:39 +0100

    test: test only exported functions of api package

--- pkg/api/apis_test.go
+++ pkg/api/apis_test.go
@@ -16,38 +16,40 @@
  * limitations under the License.
  */
 
-package api
+package api_test
 
 import (
+	"testing"
+
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/api"
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/converter/v1environment"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 var testDevEnvironment = v1environment.NewEnvironmentV1("development", "Dev", "", "https://url/to/dev/environment", "DEV")
 
 func TestNewApis(t *testing.T) {
-	apis := NewAPIs()
+	apis := api.NewAPIs()
 
 	assert.Contains(t, apis, "notification", "Expected `notification` key in KnownApis")
 	assert.Equal(t, "https://url/to/dev/environment/api/config/v1/notifications", apis["notification"].CreateURL(testDevEnvironment.GetEnvironmentUrl()), "Expected to get `notification` API url")
 }
 
 func TestContains(t *testing.T) {
-	apis := NewAPIs()
+	apis := api.NewAPIs()
 	assert.True(t, apis.Contains("alerting-profile"))
 	assert.False(t, apis.Contains("something"))
 
-	assert.False(t, APIs{}.Contains("something"))
+	assert.False(t, api.APIs{}.Contains("something"))
 }
 
 func TestApiMapFilter(t *testing.T) {
 	type given struct {
-		apis    APIs
-		filters []Filter
+		apis    api.APIs
+		filters []api.Filter
 	}
 	type expected struct {
-		apis APIs
+		apis api.APIs
 	}
 	tests := []struct {
 		name     string
@@ -57,109 +59,109 @@ func TestApiMapFilter(t *testing.T) {
 		{
 			name: "without filter",
 			given: given{
-				apis: APIs{
-					"api_1": API{ID: "api_1"},
-					"api_2": API{ID: "api_2"},
+				apis: api.APIs{
+					"api_1": api.API{ID: "api_1"},
+					"api_2": api.API{ID: "api_2"},
 				},
 				filters: nil,
 			},
 			expected: expected{
-				apis: APIs{
-					"api_1": API{ID: "api_1"},
-					"api_2": API{ID: "api_2"},
+				apis: api.APIs{
+					"api_1": api.API{ID: "api_1"},
+					"api_2": api.API{ID: "api_2"},
 				},
 			},
 		},
 		{
 			name: "filter with one filter",
 			given: given{
-				apis: APIs{
-					"api_1": API{ID: "api_1"},
-					"api_2": API{ID: "api_2"},
+				apis: api.APIs{
+					"api_1": api.API{ID: "api_1"},
+					"api_2": api.API{ID: "api_2"},
 				},
-				filters: []Filter{
-					func(api API) bool { return api.ID == "api_1" },
+				filters: []api.Filter{
+					func(api api.API) bool { return api.ID == "api_1" },
 				},
 			},
 			expected: expected{
-				apis: APIs{
-					"api_2": API{ID: "api_2"},
+				apis: api.APIs{
+					"api_2": api.API{ID: "api_2"},
 				},
 			},
 		},
 		{
 			name: "filter with two filters",
 			given: given{
-				apis: APIs{
-					"api_1": API{ID: "api_1"},
-					"api_2": API{ID: "api_2"},
+				apis: api.APIs{
+					"api_1": api.API{ID: "api_1"},
+					"api_2": api.API{ID: "api_2"},
 				},
-				filters: []Filter{
-					Filter(func(api API) bool { return api.ID == "api_1" }),
-					Filter(func(api API) bool { return api.ID == "api_2" }),
+				filters: []api.Filter{
+					api.Filter(func(api api.API) bool { return api.ID == "api_1" }),
+					api.Filter(func(api api.API) bool { return api.ID == "api_2" }),
 				},
 			},
 			expected: expected{
-				apis: APIs{},
+				apis: api.APIs{},
 			},
 		},
 		{
 			name: "noFilter",
 			given: given{
-				apis: APIs{
-					"api_1": API{ID: "api_1"},
-					"api_2": API{ID: "api_2"},
+				apis: api.APIs{
+					"api_1": api.API{ID: "api_1"},
+					"api_2": api.API{ID: "api_2"},
 				},
-				filters: []Filter{noFilter},
+				filters: []api.Filter{api.NoFilter},
 			},
 			expected: expected{
-				apis: APIs{
-					"api_1": API{ID: "api_1"},
-					"api_2": API{ID: "api_2"},
+				apis: api.APIs{
+					"api_1": api.API{ID: "api_1"},
+					"api_2": api.API{ID: "api_2"},
 				},
 			},
 		},
 		{
 			name: "RetainByName - without arguments",
 			given: given{
-				apis: APIs{
-					"api_1": API{ID: "api_1"},
-					"api_2": API{ID: "api_2"},
+				apis: api.APIs{
+					"api_1": api.API{ID: "api_1"},
+					"api_2": api.API{ID: "api_2"},
 				},
-				filters: []Filter{RetainByName([]string{})},
+				filters: []api.Filter{api.RetainByName([]string{})},
 			},
 			expected: expected{
-				apis: APIs{
-					"api_1": API{ID: "api_1"},
-					"api_2": API{ID: "api_2"},
+				apis: api.APIs{
+					"api_1": api.API{ID: "api_1"},
+					"api_2": api.API{ID: "api_2"},
 				},
 			},
 		},
 		{
 			name: "RetainByName - with arguments",
 			given: given{
-				apis: APIs{
-					"api_1": API{ID: "api_1"},
-					"api_2": API{ID: "api_2"},
+				apis: api.APIs{
+					"api_1": api.API{ID: "api_1"},
+					"api_2": api.API{ID: "api_2"},
 				},
-				filters: []Filter{RetainByName([]string{"api_1"})},
+				filters: []api.Filter{api.RetainByName([]string{"api_1"})},
 			},
 			expected: expected{
-				apis: APIs{
-					"api_1": API{ID: "api_1"},
+				apis: api.APIs{
+					"api_1": api.API{ID: "api_1"},
 				},
 			},
 		}, {
 			name: "RetainByName - with non existing argument",
 			given: given{
-				apis: APIs{
-					"api_1": API{ID: "api_1"},
-					"api_2": API{ID: "api_2"},
+				apis: api.APIs{
+					"api_1": api.API{ID: "api_1"},
+					"api_2": api.API{ID: "api_2"},
 				},
-				filters: []Filter{RetainByName([]string{"api_3"})},
+				filters: []api.Filter{api.RetainByName([]string{"api_3"})},
 			},
 			expected: expected{
-				apis: APIs{},
+				apis: api.APIs{},
 			},
 		},
 	}
---
+++ pkg/api/export_test.go
@@ -0,0 +1,41 @@
+/*
+ * @license
+ * Copyright 2024 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package api
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	ConfigEndpoints   = configEndpoints
+	ConfigEndpointsV1 = configEndpointsV1
+)
+
+func (a API) TestConfiguredApi(t *testing.T) {
+	assert.NotEmptyf(t, a.ID, "endpoint %+v have empty ID!", a)
+	if a.SingleConfiguration == true {
+		assert.Emptyf(t, a.PropertyNameOfGetAllResponse, "endpoint %q have forbiden value combination - when \"SingleConfiguration\" is true, \"PropertyNameOfGetAllResponse\" must be empty! (actual values: %+v)", a.ID, a)
+		assert.Falsef(t, a.NonUniqueName, "endpoint %q have forbiden value combination - when \"SingleConfiguration\" is true, \"NonUniqueName\" must be false! (actual values: %+v)", a.ID, a)
+	}
+}
+
+// noFilter is dummy filter that do nothing.
+func NoFilter(a API) bool {
+	return noFilter(a)
+}
