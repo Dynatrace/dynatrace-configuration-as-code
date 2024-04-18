20068afee665c6d0fe6c079b4d81f201f3fb4d6a
Bernd Warmuth <bernd.warmuth@dynatrace.com>
2024-04-18 16:57:07 +0200

    fix: delete automation resources in order

--- pkg/delete/delete.go
+++ pkg/delete/delete.go
@@ -46,37 +46,56 @@ type DeleteEntries = map[configurationType][]pointer.DeletePointer
 
 // Configs removes all given entriesToDelete from the Dynatrace environment the given client connects to
 func Configs(ctx context.Context, clients ClientSet, apis api.APIs, automationResources map[string]config.AutomationResource, entriesToDelete DeleteEntries) error {
-	deleteErrors := 0
-	for entryType, entries := range entriesToDelete {
-		if entryType == api.DashboardShareSettings {
-			log.Warn("Classic config of type %s cannot be deleted. Note, that they can be removed by deleting the associated dashboard.", api.DashboardShareSettings)
+	var deleteErrors int
+
+	// Delete automation resources (in the specified order
+	automationTypeOrder := []string{"workflow", "scheduling-rule", "business-calendar"}
+	for _, key := range automationTypeOrder {
+		entries := entriesToDelete[key]
+		if reflect.ValueOf(clients.Automation).IsNil() {
+			log.WithCtxFields(ctx).WithFields(field.Type(key)).Warn("Skipped deletion of %d Automation configuration(s) of type %q as API client was unavailable.", len(entries), key)
+			delete(entriesToDelete, key)
 			continue
 		}
-
-		var err error
-
-		if theAPI, isClassicAPI := apis[entryType]; isClassicAPI {
-			err = classic.Delete(ctx, clients.Classic, theAPI, entries)
-		} else if targetAutomation, isAutomationAPI := automationResources[entryType]; isAutomationAPI {
-			if reflect.ValueOf(clients.Automation).IsNil() {
-				log.WithCtxFields(ctx).WithFields(field.Type(entryType)).Warn("Skipped deletion of %d Automation configuration(s) of type %q as API client was unavailable.", len(entries), entryType)
-				continue
-			}
-			err = automation.Delete(ctx, clients.Automation, targetAutomation, entries)
-		} else if entryType == "bucket" {
-			if reflect.ValueOf(clients.Buckets).IsNil() {
-				log.WithCtxFields(ctx).WithFields(field.Type(entryType)).Warn("Skipped deletion of %d Grail Bucket configuration(s) as API client was unavailable.", len(entries))
-				continue
-			}
-			err = bucket.Delete(ctx, clients.Buckets, entries)
-		} else { // assume it's a Settings Schema
-			err = setting.Delete(ctx, clients.Settings, entries)
-		}
-
+		err := automation.Delete(ctx, clients.Automation, automationResources[key], entries)
 		if err != nil {
 			log.WithFields(field.Error(err)).Error("Error during deletion: %v", err)
 			deleteErrors += 1
 		}
+		delete(entriesToDelete, key)
+	}
+
+	// Delete bucket resources
+	if reflect.ValueOf(clients.Buckets).IsNil() {
+		log.WithCtxFields(ctx).WithFields(field.Type("bucket")).Warn("Skipped deletion of %d Grail Bucket configuration(s) as API client was unavailable.", len(entriesToDelete["bucket"]))
+	} else {
+		if err := bucket.Delete(ctx, clients.Buckets, entriesToDelete["bucket"]); err != nil {
+			log.WithFields(field.Error(err)).Error("Error during deletion: %v", err)
+			deleteErrors += 1
+		}
+		delete(entriesToDelete, "bucket")
+	}
+
+	// Dashboard share settings cannot be deleted
+	if _, ok := entriesToDelete[api.DashboardShareSettings]; ok {
+		log.Warn("Classic config of type %s cannot be deleted. Note, that they can be removed by deleting the associated dashboard.", api.DashboardShareSettings)
+		delete(entriesToDelete, api.DashboardShareSettings)
+
+	}
+
+	// Delete classic configs and settings
+	for entryType, entries := range entriesToDelete {
+		var err error
+		if theAPI, isClassicAPI := apis[entryType]; isClassicAPI {
+			err = classic.Delete(ctx, clients.Classic, theAPI, entries)
+		} else { // assume it's a Settings Schema
+			err = setting.Delete(ctx, clients.Settings, entries)
+		}
+		if err != nil {
+			log.WithFields(field.Error(err)).Error("Error during deletion: %v", err)
+			deleteErrors += 1
+		}
+		delete(entriesToDelete, entryType)
 	}
 
 	if deleteErrors > 0 {
