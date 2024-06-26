df88fe8c70a545f062c3fd29d5667e7bd1768708
UnseenWizzard <nicola.riedmann@dynatrace.com>
2024-06-26 14:45:46 +0200

    refactor(poc): Move feature flags from functions to map
    This makes access more tedious, but allows us to iterate over defined
    flags and produce an overview.

--- internal/featureflags/temporary.go
+++ internal/featureflags/temporary.go
@@ -16,25 +16,49 @@
 
 package featureflags
 
-// VerifyEnvironmentType returns the feature flag that tells whether the environment check
-// at the beginning of execution is enabled or not.
-// Introduced: before 2023-04-27; v2.0.0
-func VerifyEnvironmentType() FeatureFlag {
-	return FeatureFlag{
+type Flag int
+
+const (
+	VerifyEnvironmentType Flag = iota
+	ManagementZoneSettingsNumericIDs
+)
+
+var Temporary = map[Flag]FeatureFlag{
+	// VerifyEnvironmentType returns the feature flag that tells whether the environment check
+	// at the beginning of execution is enabled or not.
+	// Introduced: before 2023-04-27; v2.0.0
+	VerifyEnvironmentType: {
 		envName:        "MONACO_FEAT_VERIFY_ENV_TYPE",
 		defaultEnabled: true,
-	}
-}
-
-// ManagementZoneSettingsNumericIDs returns the feature flag that tells whether configs of settings type builtin:management-zones
-// are addressed directly via their object ID or their resolved numeric ID when they are referenced.
-// Introduced: 2023-04-18; v2.0.1
-func ManagementZoneSettingsNumericIDs() FeatureFlag {
-	return FeatureFlag{
+	},
+	// ManagementZoneSettingsNumericIDs returns the feature flag that tells whether configs of settings type builtin:management-zones
+	// are addressed directly via their object ID or their resolved numeric ID when they are referenced.
+	// Introduced: 2023-04-18; v2.0.1
+	ManagementZoneSettingsNumericIDs: {
 		envName:        "MONACO_FEAT_USE_MZ_NUMERIC_ID",
 		defaultEnabled: true,
-	}
+	},
 }
+
+//// VerifyEnvironmentType returns the feature flag that tells whether the environment check
+//// at the beginning of execution is enabled or not.
+//// Introduced: before 2023-04-27; v2.0.0
+//func VerifyEnvironmentType() FeatureFlag {
+//	return FeatureFlag{
+//		envName:        "MONACO_FEAT_VERIFY_ENV_TYPE",
+//		defaultEnabled: true,
+//	}
+//}
+//
+//// ManagementZoneSettingsNumericIDs returns the feature flag that tells whether configs of settings type builtin:management-zones
+//// are addressed directly via their object ID or their resolved numeric ID when they are referenced.
+//// Introduced: 2023-04-18; v2.0.1
+//func ManagementZoneSettingsNumericIDs() FeatureFlag {
+//	return FeatureFlag{
+//		envName:        "MONACO_FEAT_USE_MZ_NUMERIC_ID",
+//		defaultEnabled: true,
+//	}
+//}
 
 // ConsistentUUIDGeneration returns the feature flag controlling whether generated UUIDs use consistent separator characters regardless of OS
 // This is default true and just exists to get old, technically buggy behavior on Windows again if needed.
