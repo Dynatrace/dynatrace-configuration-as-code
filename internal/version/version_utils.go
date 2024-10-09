fd6ddc6f36ce509faa3d4de45a7d86cb488d58ba
Arthur Pitman <arthur.pitman@dynatrace.com>
2024-10-09 16:32:23 +0200

    chore: Update description of `ParseVersion(...)`

--- internal/version/version_utils.go
+++ internal/version/version_utils.go
@@ -71,9 +71,10 @@ func (v Version) Invalid() bool {
 
 }
 
-// ParseVersion takes a version as string and tries to parse it to convert it to a
-// Version value. It returns the Version value and possibly an error if the string could not be parsed
-// according to the expected format: "MAJOR.MINOR.PATCH" or "MAJOR.MINOR" each component being a non-negative number
+// ParseVersion takes a version as string and tries to parse it to convert it to a Version value.
+// It returns the Version value and possibly an error if the string could not be parsed.
+// Expected formats are "MAJOR", "MAJOR.MINOR" or "MAJOR.MINOR.PATCH" with each component being a non-negative number.
+// Omitted minor or patch versions are interpreted as 0, so "2" is interpreted as 2.0.0 and "2.1" is interpreted as 2.1.0.
 func ParseVersion(versionString string) (Version, error) {
 	split := strings.Split(versionString, ".")
 	if len(split) < 1 || len(split) > 3 {
