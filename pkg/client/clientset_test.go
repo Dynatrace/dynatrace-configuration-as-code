ade52728c6b7ba5d2d48ce9f9e6085562d62177b
tomaz.puhar <tomaz.puhar@dynatrace.com>
2024-10-24 14:38:45 +0200

    refactor: cleanup and add clientset_test

--- pkg/client/clientset_test.go
+++ pkg/client/clientset_test.go
@@ -17,9 +17,98 @@
 package client
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/cmd/monaco/support"
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/manifest"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/oauth2"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestCreateClientSet(t *testing.T) {
-	//@TODO implement tests for token and oauth, token, oauth
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		if strings.HasSuffix(req.URL.Path, "sso") {
+			token := &oauth2.Token{
+				AccessToken: "test-access-token",
+				TokenType:   "Bearer",
+				Expiry:      time.Now().Add(time.Hour),
+			}
+
+			rw.Header().Set("Content-Type", "application/json")
+			_ = json.NewEncoder(rw).Encode(token)
+			return
+		}
+
+		rw.WriteHeader(200)
+		output := fmt.Sprintf(`{"version" : "0.59.3.20231603", "domain": "http://%s/api/test", "endpoint": "http://%s/api/test"}`, req.Host, req.Host)
+		_, _ = rw.Write([]byte(output))
+	}))
+	defer server.Close()
+
+	tests := []struct {
+		name string
+		url  string
+		auth manifest.Auth
+	}{
+		{"token client set",
+			server.URL,
+			manifest.Auth{
+				Token: &manifest.AuthSecret{
+					Name:  "token-env-var",
+					Value: "mock token",
+				},
+			},
+		},
+		{"oAuth client set",
+			server.URL,
+			manifest.Auth{
+				OAuth: &manifest.OAuth{
+					ClientID: manifest.AuthSecret{
+						Name:  "client-id",
+						Value: "resolved-client-id",
+					},
+					ClientSecret: manifest.AuthSecret{
+						Name:  "client-secret",
+						Value: "resolved-client-secret",
+					},
+					TokenEndpoint: &manifest.URLDefinition{
+						Value: server.URL + "/sso",
+					},
+				},
+			},
+		},
+		{"oAuth and token client set",
+			server.URL,
+			manifest.Auth{
+				Token: &manifest.AuthSecret{
+					Name:  "token-env-var",
+					Value: "mock token",
+				},
+				OAuth: &manifest.OAuth{
+					ClientID: manifest.AuthSecret{
+						Name:  "client-id",
+						Value: "resolved-client-id",
+					},
+					ClientSecret: manifest.AuthSecret{
+						Name:  "client-secret",
+						Value: "resolved-client-secret",
+					},
+					TokenEndpoint: &manifest.URLDefinition{
+						Value: server.URL + "/sso",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := CreateClientSet(tt.url, tt.auth, ClientOptions{SupportArchive: support.SupportArchive})
+			assert.NoError(t, err)
+		})
+	}
 }
