8455bdf8f4760bb33bf9a05861fdd8c9cbdb9542
Jure Skelin <jure.skelin@dynatrace.com>
2024-12-03 08:10:31 +0100

    feat: enable reading and writing of grail filter-segment

--- cmd/monaco/download/download_integration_test.go
+++ cmd/monaco/download/download_integration_test.go
@@ -1,6 +1,6 @@
 //go:build unit
 
-//@license
+// @license
 // Copyright 2022 Dynatrace LLC
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
