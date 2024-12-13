c1575168c9cc10882e0632f1cc27e804eb57929a
Jure Skelin <jure.skelin@dynatrace.com>
2024-12-13 15:14:36 +0100

    feat(segment): add deleteAll for segments (monaco purge)

--- pkg/delete/delete.go
+++ pkg/delete/delete.go
@@ -165,6 +165,15 @@ func All(ctx context.Context, clients client.ClientSet, apis api.APIs) error {
 		}
 	}
 
+	if featureflags.Temporary[featureflags.Segments].Enabled() {
+		if clients.SegmentClient == nil {
+			log.Warn("Skipped deletion of %s configurations as appropriate client was unavailable.", config.SegmentID)
+		} else if err := segment.DeleteAll(ctx, clients.SegmentClient); err != nil {
+			log.Error("Failed to delete all %s configurations: %v", config.SegmentID, err)
+			errCount++
+		}
+	}
+
 	if errCount > 0 {
 		return fmt.Errorf("failed to delete all configurations for %d types", errCount)
 	}
