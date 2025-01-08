66ed138af55577b235c2f13b0af7c5139bdd1d00
Jure Skelin <jure.skelin@dynatrace.com>
2025-01-09 00:09:32 +0100

    test(performance): test micro-set

---
+++ .ci/jenkins/performance_pipeline.groovy
@@ -0,0 +1,69 @@
+podTemplate(yaml: readTrusted('.ci/jenkins/agents/build-agent.yaml')) {
+    node(POD_LABEL) {
+        stage("get source") {
+            checkout scm
+        }
+        monaco = load(".ci/jenkins/tools/monaco.groovy")
+
+        container("monaco-build") {
+            stage("🏗️ building") {
+                monaco.build(".")
+                cleanWs()
+            }
+            stage("get test data") {
+                git(credentialsId: 'bitbucket-buildmaster',
+                    url: 'https://bitbucket.lab.dynatrace.org/scm/claus/monaco-test-data.git',
+                    branch: 'main')
+            }
+            stage('purge tenant') {
+                monaco.purge()
+            }
+        }
+
+        try {
+            container("monaco-runner") {
+                stage("test") {
+                    monaco.deploy("micro-set")
+                }
+            }
+        } finally {
+            container("monaco-build") {
+                stage('purge tenant') {
+                    monaco.purge()
+                }
+            }
+        }
+    }
+}
+
+void buildMonaco() {
+    stage('build monaco') {
+        sh "CGO_ENABLED=0 go build -buildvcs=false -a -tags netgo -ldflags \"-X github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/version.MonitoringAsCode=2.x -w -extldflags -static\" -o $JENKINS_AGENT_WORKDIR/monaco ./cmd/monaco"
+    }
+}
+
+
+void monacoBuild(String sourcePath) {
+    String monacoBin = "${JENKINS_AGENT_WORKDIR}/monaco"
+    sh(label: "build monaco",
+        script: """CGO_ENABLED=0
+                go build
+                  -a -tags=netgo -buildvcs=false
+                  -ldflags=\"-X github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/version.MonitoringAsCode=2.x -w -extldflags -static\"
+                  -o ${monacoBin}
+                  ${sourcePath}/cmd/monaco
+            """.replaceAll("\n", " ").replaceAll(/ +/, " "))
+
+}
+
+void monacoPurge() {
+    String monacoBin = "${JENKINS_AGENT_WORKDIR}/monaco"
+    sh(label: "purge tenant",
+        script: "MONACO_ENABLE_DANGEROUS_COMMANDS=true ${monacoBin} purge --help")
+}
+
+void monacoDeploy() {
+    String monacoBin = "${JENKINS_AGENT_WORKDIR}/monaco"
+    sh(label: "monaco deploy",
+        script: " ${monacoBin} deploy --help")
+}
