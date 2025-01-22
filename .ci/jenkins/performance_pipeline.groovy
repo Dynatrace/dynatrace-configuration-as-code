d0ccc128894dce33465e19e124caa328867cd26a
Jure Skelin <jure.skelin@dynatrace.com>
2025-01-22 12:55:44 +0100

    test(performance): remove unused methods

--- .ci/jenkins/performance_pipeline.groovy
+++ .ci/jenkins/performance_pipeline.groovy
@@ -37,35 +37,3 @@ podTemplate(yaml: readTrusted('.ci/jenkins/agents/performance-agent.yaml')) {
         }
     }
 }
-
-void buildMonaco() {
-    stage('build monaco') {
-        sh "CGO_ENABLED=0 go build -buildvcs=false -a -tags netgo -ldflags \"-X github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/version.MonitoringAsCode=2.x -w -extldflags -static\" -o $JENKINS_AGENT_WORKDIR/monaco ./cmd/monaco"
-    }
-}
-
-
-void monacoBuild(String sourcePath) {
-    String monacoBin = "${JENKINS_AGENT_WORKDIR}/monaco"
-    sh(label: "build monaco",
-        script: """CGO_ENABLED=0
-                go build
-                  -a -tags=netgo -buildvcs=false
-                  -ldflags=\"-X github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/version.MonitoringAsCode=2.x -w -extldflags -static\"
-                  -o ${monacoBin}
-                  ${sourcePath}/cmd/monaco
-            """.replaceAll("\n", " ").replaceAll(/ +/, " "))
-
-}
-
-void monacoPurge() {
-    String monacoBin = "${JENKINS_AGENT_WORKDIR}/monaco"
-    sh(label: "purge tenant",
-        script: "MONACO_ENABLE_DANGEROUS_COMMANDS=true ${monacoBin} purge --help")
-}
-
-void monacoDeploy() {
-    String monacoBin = "${JENKINS_AGENT_WORKDIR}/monaco"
-    sh(label: "monaco deploy",
-        script: " ${monacoBin} deploy --help")
-}
