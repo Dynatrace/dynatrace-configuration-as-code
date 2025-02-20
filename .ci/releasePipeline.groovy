fa8f5fb13b0f0c4891438e43a5c64d5dd1ab4ea3
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2025-02-20 10:26:26 +0100

    chore(jenkins): update image and security context of container

--- .ci/releasePipeline.groovy
+++ .ci/releasePipeline.groovy
@@ -2,9 +2,9 @@ pipeline {
     agent {
         kubernetes {
             cloud 'linux-amd64'
-            nodeSelector 'kubernetes.io/arch=amd64,kubernetes.io/os=linux'
             instanceCap '2'
             yamlFile '.ci/jenkins_agents/ca-jenkins-agent.yaml'
+            defaultContainer "ca-jenkins-agent"
         }
     }
 
