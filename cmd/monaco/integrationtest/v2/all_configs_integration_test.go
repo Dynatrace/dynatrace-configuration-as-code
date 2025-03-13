cfcdedf8e56acee5c42238c415e923a6da32f400
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2025-03-13 13:05:13 +0100

    feat(settings-acl): Add E2E

--- cmd/monaco/integrationtest/v2/all_configs_integration_test.go
+++ cmd/monaco/integrationtest/v2/all_configs_integration_test.go
@@ -40,6 +40,7 @@ func TestIntegrationAllConfigsClassic(t *testing.T) {
 	// flags are needed because the configs are still read and invalid types result in an error
 	t.Setenv(featureflags.OpenPipeline.EnvName(), "true")
 	t.Setenv(featureflags.ServiceLevelObjective.EnvName(), "true")
+	t.Setenv(featureflags.AccessControlSettings.EnvName(), "true")
 	targetEnvironment := "classic_env"
 
 	RunIntegrationWithCleanup(t, configFolder, manifest, targetEnvironment, "AllConfigs", func(fs afero.Fs, _ TestContext) {
@@ -57,6 +58,7 @@ func TestIntegrationAllConfigsPlatform(t *testing.T) {
 
 	t.Setenv(featureflags.OpenPipeline.EnvName(), "true")
 	t.Setenv(featureflags.ServiceLevelObjective.EnvName(), "true")
+	t.Setenv(featureflags.AccessControlSettings.EnvName(), "true")
 
 	targetEnvironment := "platform_env"
 
