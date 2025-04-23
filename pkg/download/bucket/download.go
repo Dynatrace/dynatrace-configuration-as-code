67ed64343d850ad8f377e6cc268adb679e9c81ef
Dominic Weinberger <dominic.weinberger@dynatrace.com>
2025-04-23 16:36:23 +0200

    refactor(download): move templatetools from "pkg/download/internal" to "internal"
    This prepares a refactoring PR with the goal of making the download code independent of the concrete feature/API

--- pkg/download/bucket/download.go
+++ pkg/download/bucket/download.go
@@ -26,12 +26,12 @@ import (
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/internal/log"
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/internal/log/field"
 	escTemplate "github.com/dynatrace/dynatrace-configuration-as-code/v2/internal/template"
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/internal/templatetools"
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/client"
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/config"
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/config/coordinate"
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/config/parameter"
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/config/template"
-	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/download/internal/templatetools"
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/project"
 )
 
