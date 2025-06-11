46faddece70fd290174cb46e2b9951590815be8b
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2025-06-11 12:39:49 +0200

    refactor(deploy): generify openpipeline

--- pkg/resource/openpipeline/deploy.go
+++ pkg/resource/openpipeline/deploy.go
@@ -1,6 +1,6 @@
 /*
  * @license
- * Copyright 2024 Dynatrace LLC
+ * Copyright 2025 Dynatrace LLC
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -32,12 +32,20 @@ import (
 	deployErrors "github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/deploy/errors"
 )
 
-//go:generate mockgen -source=openpipeline.go -destination=openpipeline_mock.go -package=openpipeline openpipelineClient
-type Client interface {
+//go:generate mockgen -source=deploy.go -destination=openpipeline_mock.go -package=openpipeline DeploySource
+type DeploySource interface {
 	Update(ctx context.Context, id string, data []byte) (openpipeline.Response, error)
 }
 
-func Deploy(ctx context.Context, client Client, properties parameter.Properties, renderedConfig string, c *config.Config) (entities.ResolvedEntity, error) {
+type DeployAPI struct {
+	source DeploySource
+}
+
+func NewDeployAPI(source DeploySource) *DeployAPI {
+	return &DeployAPI{source}
+}
+
+func (d DeployAPI) Deploy(ctx context.Context, properties parameter.Properties, renderedConfig string, c *config.Config) (entities.ResolvedEntity, error) {
 	//create new context to carry logger
 	ctx = logr.NewContextWithSlogLogger(ctx, slog.Default())
 	ctx, cancel := context.WithTimeout(ctx, time.Minute)
@@ -48,7 +56,7 @@ func Deploy(ctx context.Context, client Client, properties parameter.Properties,
 		return entities.ResolvedEntity{}, fmt.Errorf("expected openpipeline config type but found %v", t)
 	}
 
-	_, err := client.Update(ctx, t.Kind, []byte(renderedConfig))
+	_, err := d.source.Update(ctx, t.Kind, []byte(renderedConfig))
 	if err != nil {
 		return entities.ResolvedEntity{}, deployErrors.NewConfigDeployErr(c, fmt.Sprintf("failed to update openpipeline object of kind '%s'", t.Kind)).WithError(err)
 	}
