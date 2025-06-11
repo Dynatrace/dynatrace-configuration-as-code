77ba3cffb7e0f6d2b7e89c05dcf0409774292c9e
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2025-06-11 11:57:12 +0200

    refactor(deploy): generify segments

--- pkg/resource/segment/deploy.go
+++ pkg/resource/segment/deploy.go
@@ -1,6 +1,6 @@
 /*
  * @license
- * Copyright 2024 Dynatrace LLC
+ * Copyright 2025 Dynatrace LLC
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -33,7 +33,7 @@ import (
 	deployErrors "github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/deploy/errors"
 )
 
-type deploySegmentClient interface {
+type DeploySource interface {
 	Update(ctx context.Context, id string, data []byte) (api.Response, error)
 	Create(ctx context.Context, data []byte) (api.Response, error)
 	GetAll(ctx context.Context) ([]api.Response, error)
@@ -43,7 +43,15 @@ type jsonResponse struct {
 	ExternalId string `json:"externalId"`
 }
 
-func Deploy(ctx context.Context, client deploySegmentClient, properties parameter.Properties, renderedConfig string, c *config.Config) (entities.ResolvedEntity, error) {
+type DeployAPI struct {
+	source DeploySource
+}
+
+func NewDeployAPI(source DeploySource) *DeployAPI {
+	return &DeployAPI{source: source}
+}
+
+func (d DeployAPI) Deploy(ctx context.Context, properties parameter.Properties, renderedConfig string, c *config.Config) (entities.ResolvedEntity, error) {
 	ctx = logr.NewContextWithSlogLogger(ctx, slog.Default())
 	ctx, cancel := context.WithTimeout(ctx, time.Minute)
 	defer cancel()
@@ -56,7 +64,7 @@ func Deploy(ctx context.Context, client deploySegmentClient, properties paramete
 
 	//Strategy 1 when OriginObjectId is set we update the object
 	if c.OriginObjectId != "" {
-		_, err := client.Update(ctx, c.OriginObjectId, requestPayload)
+		_, err := d.source.Update(ctx, c.OriginObjectId, requestPayload)
 		if err == nil {
 			return createResolveEntity(c.OriginObjectId, properties, c), nil
 		}
@@ -67,13 +75,13 @@ func Deploy(ctx context.Context, client deploySegmentClient, properties paramete
 	}
 
 	//Strategy 2 is to try to find a match with external id and update it
-	matchData, match, err := findMatchOnRemote(ctx, client, externalId)
+	matchData, match, err := d.findMatchOnRemote(ctx, externalId)
 	if err != nil {
 		return entities.ResolvedEntity{}, deployErrors.NewConfigDeployErr(c, fmt.Sprintf("error finding segment with externalId: %s", externalId)).WithError(err)
 	}
 
 	if match {
-		_, err := client.Update(ctx, matchData.UID, requestPayload)
+		_, err := d.source.Update(ctx, matchData.UID, requestPayload)
 		if err != nil {
 			return entities.ResolvedEntity{}, deployErrors.NewConfigDeployErr(c, fmt.Sprintf("failed to update segment with externalId: %s", externalId)).WithError(err)
 		}
@@ -81,7 +89,7 @@ func Deploy(ctx context.Context, client deploySegmentClient, properties paramete
 	}
 
 	//Strategy 3 is to create a new segment object
-	createResponse, err := client.Create(ctx, requestPayload)
+	createResponse, err := d.source.Create(ctx, requestPayload)
 	if err != nil {
 		return entities.ResolvedEntity{}, deployErrors.NewConfigDeployErr(c, fmt.Sprintf("failed to create segment with externalId: %s", externalId)).WithError(err)
 	}
@@ -104,8 +112,8 @@ func addExternalId(externalId string, renderedConfig string) ([]byte, error) {
 	return json.Marshal(request)
 }
 
-func findMatchOnRemote(ctx context.Context, client deploySegmentClient, externalId string) (jsonResponse, bool, error) {
-	segmentsResponses, err := client.GetAll(ctx)
+func (d DeployAPI) findMatchOnRemote(ctx context.Context, externalId string) (jsonResponse, bool, error) {
+	segmentsResponses, err := d.source.GetAll(ctx)
 	if err != nil {
 		return jsonResponse{}, false, fmt.Errorf("failed to GET segments: %w", err)
 	}
