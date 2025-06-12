68bc8dc22044effaf35b7c86dc4fbb92cc723a11
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2025-06-12 13:15:04 +0200

    refactor: move extract config utils to resource

--- pkg/deploy/internal/classic/classic.go
+++ pkg/deploy/internal/classic/classic.go
@@ -32,7 +32,7 @@ import (
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/config/entities"
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/config/parameter"
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/deploy/errors"
-	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/deploy/internal/extract"
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/resource/extract"
 )
 
 func Deploy(ctx context.Context, configClient client.ConfigClient, apis api.APIs, properties parameter.Properties, renderedConfig string, conf *config.Config) (entities.ResolvedEntity, error) {
