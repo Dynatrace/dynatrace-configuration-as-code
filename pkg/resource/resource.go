76c3c4775804800c24bb643c86a682d044897c2d
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2025-06-17 12:11:19 +0200

    refactor(deploy): make use of generic deployable

--- pkg/resource/resource.go
+++ pkg/resource/resource.go
@@ -25,6 +25,8 @@ import (
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/project"
 )
 
+type Deployables = map[config.TypeID]Deployable
+
 type Deployable interface {
 	// Deploy deploys a given resource and returns the resolved entity
 	Deploy(ctx context.Context, properties parameter.Properties, renderedConfig string, c *config.Config) (entities.ResolvedEntity, error)
