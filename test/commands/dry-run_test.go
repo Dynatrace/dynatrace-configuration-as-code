55beefa400fd15d50c27414945644e3765eafd2c
Dominic Weinberger <dominic.weinberger@dynatrace.com>
2025-06-25 10:54:44 +0200

    feat: Adds dry-run test for platform tokens

--- test/commands/dry-run_test.go
+++ test/commands/dry-run_test.go
@@ -32,39 +32,59 @@ import (
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/test/internal/runner"
 )
 
-func TestDryRun(t *testing.T) {
-	specificEnvironment := "platform_env"
+func TestDryRunWithOAuth(t *testing.T) {
 	configFolder := "testdata/integration-all-configs/"
 	manifest := configFolder + "manifest.yaml"
-
-	envVars := map[string]string{
-		featureflags.ServiceLevelObjective.EnvName(): "true",
-	}
 
 	runner.Run(t, configFolder,
 		runner.Options{
 			runner.WithManifestPath(manifest),
 			runner.WithSuffix("AllConfigs"),
-			runner.WithEnvironment(specificEnvironment),
-			runner.WithEnvVars(envVars),
+			runner.WithEnvironment("platform_env"),
+			runner.WithEnvVars(map[string]string{
+				featureflags.ServiceLevelObjective.EnvName(): "true",
+			}),
 		},
 		func(fs afero.Fs, _ runner.TestContext) {
-			server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-				t.Fatalf("unexpected HTTP request made during dry run: %s", req.RequestURI)
-			}))
-			defer server.Close()
-
-			// ensure all URLs used in the manifest point at the test server
-			setAllURLEnvironmentVariables(t, server.URL)
-
-			// This causes a POST for all configs:
-			err := monaco.Run(t, fs, fmt.Sprintf("monaco deploy %s --environment=%s --verbose --dry-run", manifest, specificEnvironment))
-			assert.NoError(t, err)
-
-			// This causes a PUT for all configs:
-			err = monaco.Run(t, fs, fmt.Sprintf("monaco deploy %s --environment=%s --verbose --dry-run", manifest, specificEnvironment))
-			assert.NoError(t, err)
+			dryRun(t, fs, manifest, "platform_env")
 		})
+}
+
+func TestDryRunWithPlatformToken(t *testing.T) {
+	configFolder := "testdata/integration-all-configs/"
+	manifest := configFolder + "manifest.yaml"
+
+	runner.Run(t, configFolder,
+		runner.Options{
+			runner.WithManifestPath(manifest),
+			runner.WithSuffix("AllConfigs"),
+			runner.WithEnvironment("platform_token_env"),
+			runner.WithEnvVars(map[string]string{
+				featureflags.ServiceLevelObjective.EnvName(): "true",
+				featureflags.PlatformToken.EnvName():         "true",
+			}),
+		},
+		func(fs afero.Fs, _ runner.TestContext) {
+			dryRun(t, fs, manifest, "platform_token_env")
+		})
+}
+
+func dryRun(t *testing.T, fs afero.Fs, manifest string, environment string) {
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		t.Fatalf("unexpected HTTP request made during dry run: %s", req.RequestURI)
+	}))
+	defer server.Close()
+
+	// ensure all URLs used in the manifest point at the test server
+	setAllURLEnvironmentVariables(t, server.URL)
+
+	// This causes a POST for all configs:
+	err := monaco.Run(t, fs, fmt.Sprintf("monaco deploy %s --environment=%s --verbose --dry-run", manifest, environment))
+	assert.NoError(t, err)
+
+	// This causes a PUT for all configs:
+	err = monaco.Run(t, fs, fmt.Sprintf("monaco deploy %s --environment=%s --verbose --dry-run", manifest, environment))
+	assert.NoError(t, err)
 }
 
 func setAllURLEnvironmentVariables(t *testing.T, url string) {
