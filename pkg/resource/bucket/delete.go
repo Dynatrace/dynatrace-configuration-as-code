653977ccd138df64982186a1958167b59ddf440a
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2025-07-08 11:04:34 +0200

    refactor(bucket): merge delete and deleteAll similarities

--- pkg/resource/bucket/delete.go
+++ pkg/resource/bucket/delete.go
@@ -26,6 +26,7 @@ import (
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/internal/buckettools"
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/internal/idutils"
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/internal/log"
+	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/config/coordinate"
 	"github.com/dynatrace/dynatrace-configuration-as-code/v2/pkg/delete/pointer"
 )
 
@@ -43,40 +44,17 @@ func NewDeleter(bucketSource DeleteSource) *Deleter {
 	return &Deleter{bucketSource: bucketSource}
 }
 
+type bucketDelete struct {
+	bucketName string
+	coordinate *coordinate.Coordinate
+}
+
 func (d Deleter) Delete(ctx context.Context, entries []pointer.DeletePointer) error {
 	logger := log.With(log.TypeAttr("bucket"))
-	logger.InfoContext(ctx, `Deleting %d config(s) of type "bucket"...`, len(entries))
+	bucketDeletes := deletePointerToBucketDelete(entries)
 
-	deleteErrs := 0
-	for _, e := range entries {
-
-		logger := logger.With(log.CoordinateAttr(e.AsCoordinate()))
-
-		bucketName := e.OriginObjectId
-		if e.OriginObjectId == "" {
-			bucketName = idutils.GenerateBucketName(e.AsCoordinate())
-		}
-
-		logger.DebugContext(ctx, "Deleting bucket '%s'", bucketName)
-		bucketExists, err := buckets.AwaitActiveOrNotFound(ctx, d.bucketSource, bucketName, maxRetryDuration, durationBetweenRetries)
-		if err != nil {
-			logger.With(log.ErrorAttr(err)).ErrorContext(ctx, "Failed to delete Grail Bucket '%s': %v", bucketName, err)
-			deleteErrs++
-			continue
-		}
-		if !bucketExists {
-			// bucket already deleted
-			continue
-		}
-		_, err = d.bucketSource.Delete(ctx, bucketName)
-		if err != nil && !api.IsNotFoundError(err) {
-			logger.With(log.ErrorAttr(err)).ErrorContext(ctx, "Failed to delete Grail Bucket '%s': %v", bucketName, err)
-			deleteErrs++
-		}
-	}
-
-	if deleteErrs > 0 {
-		return fmt.Errorf("failed to delete %d Grail Bucket configurations", deleteErrs)
+	if errorCount := d.delete(ctx, bucketDeletes, logger); errorCount > 0 {
+		return fmt.Errorf("failed to delete %d Grail Bucket configurations", errorCount)
 	}
 
 	return nil
@@ -99,45 +77,91 @@ func (d Deleter) DeleteAll(ctx context.Context) error {
 		return err
 	}
 
-	logger.InfoContext(ctx, "Deleting %d objects of type %q...", len(response.All()), "bucket")
-	errCount := 0
-	for _, obj := range response.All() {
-		var bucketName struct {
-			BucketName string `json:"bucketName"`
+	bucketDeletes, errorCountParse := responsesToBucketDelete(ctx, response.All(), logger)
+	errorCountDelete := d.delete(ctx, bucketDeletes, logger)
+	errorCount := errorCountParse + errorCountDelete
+
+	if errorCount > 0 {
+		return fmt.Errorf("failed to delete %d Grail Bucket configuration(s)", errorCount)
+	}
+
+	return nil
+}
+
+func deletePointerToBucketDelete(entries []pointer.DeletePointer) []bucketDelete {
+	bucketsDeletes := make([]bucketDelete, len(entries))
+	for i, e := range entries {
+		cord := e.AsCoordinate()
+		bucketName := e.OriginObjectId
+
+		if e.OriginObjectId == "" {
+			bucketName = idutils.GenerateBucketName(cord)
 		}
 
+		bucketsDeletes[i] = bucketDelete{
+			bucketName: bucketName,
+			coordinate: &cord,
+		}
+	}
+	return bucketsDeletes
+}
+
+func responsesToBucketDelete(ctx context.Context, bucketResponses [][]byte, logger *log.Slogger) ([]bucketDelete, int) {
+	var bucketName struct {
+		BucketName string `json:"bucketName"`
+	}
+	bucketDeletes := make([]bucketDelete, 0)
+	errCount := 0
+
+	for _, obj := range bucketResponses {
 		if err := json.Unmarshal(obj, &bucketName); err != nil {
 			logger.ErrorContext(ctx, "Failed to parse bucket JSON: %v", err)
 			errCount++
 			continue
 		}
+		bucketDeletes = append(bucketDeletes, bucketDelete{
+			bucketName: bucketName.BucketName,
+		})
+	}
+	return bucketDeletes, errCount
+}
 
+func (d Deleter) delete(ctx context.Context, bucketDeletes []bucketDelete, baseLogger *log.Slogger) int {
+	errorCount := 0
+	baseLogger.InfoContext(ctx, `Deleting %d config(s) of type 'bucket'...`, len(bucketDeletes))
+
+	for _, bucketDeleteEntry := range bucketDeletes {
+		bucketName := bucketDeleteEntry.bucketName
 		// exclude builtin bucket names, they cannot be deleted anyway
-		if buckettools.IsDefault(bucketName.BucketName) {
+		if buckettools.IsDefault(bucketName) {
 			continue
 		}
-		// before deleting wait until it can be deleted
-		bucketExists, err := buckets.AwaitActiveOrNotFound(ctx, d.bucketSource, bucketName.BucketName, maxRetryDuration, durationBetweenRetries)
+
+		logger := baseLogger
+		if bucketDeleteEntry.coordinate != nil {
+			logger = logger.With(log.CoordinateAttr(*bucketDeleteEntry.coordinate))
+		}
+
+		logger.DebugContext(ctx, "Deleting bucket '%s'", bucketName)
+		bucketExists, err := buckets.AwaitActiveOrNotFound(ctx, d.bucketSource, bucketName, maxRetryDuration, durationBetweenRetries)
+
 		if err != nil {
-			logger.ErrorContext(ctx, "Failed to delete Grail Bucket '%s': %v", bucketName.BucketName, err)
-			errCount++
+			logger.With(log.ErrorAttr(err)).ErrorContext(ctx, "Failed to delete Grail Bucket '%s': %v", bucketName, err)
+			errorCount++
 			continue
 		}
+
 		if !bucketExists {
 			// bucket already deleted
 			continue
 		}
-		_, err = d.bucketSource.Delete(ctx, bucketName.BucketName)
+
+		_, err = d.bucketSource.Delete(ctx, bucketName)
+
 		if err != nil && !api.IsNotFoundError(err) {
-			logger.ErrorContext(ctx, "Failed to delete Grail Bucket '%s': %v", bucketName.BucketName, err)
-			errCount++
-			continue
+			logger.With(log.ErrorAttr(err)).ErrorContext(ctx, "Failed to delete Grail Bucket '%s': %v", bucketName, err)
+			errorCount++
 		}
 	}
-
-	if errCount > 0 {
-		return fmt.Errorf("failed to delete %d Grail Bucket configuration(s)", errCount)
-	}
-
-	return nil
+	return errorCount
 }
